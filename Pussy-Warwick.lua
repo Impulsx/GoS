2f769569e179b3f9fa6599cc74f63a55b9fd09f9
Pussykate <35004487+Pussykate@users.noreply.github.com>
2018-10-12 22:11:50 +0200

    Update Pussy-Warwick.lua

--- Pussy-Warwick.lua
+++ Pussy-Warwick.lua
@@ -1,6 +1,52 @@
 if myHero.charName ~= "Warwick" then return end
 
---20.01.2018--
+-- [ AutoUpdate ]
+do
+    
+    local Version = 0.01
+    
+    local Files = {
+        Lua = {
+            Path = SCRIPT_PATH,
+            Name = "PussyTristana.lua",
+            Url = "https://raw.githubusercontent.com/Pussykate/GoS/master/Pussy-Warwick.lua"
+        },
+        Version = {
+            Path = SCRIPT_PATH,
+            Name = "PussyTristana.version",
+            Url = "https://raw.githubusercontent.com/Pussykate/GoS/master/Pussy-Warwick.version"
+        }
+    }
+    
+    local function AutoUpdate()
+        
+        local function DownloadFile(url, path, fileName)
+            DownloadFileAsync(url, path .. fileName, function() end)
+            while not FileExist(path .. fileName) do end
+        end
+        
+        local function ReadFile(path, fileName)
+            local file = io.open(path .. fileName, "r")
+            local result = file:read()
+            file:close()
+            return result
+        end
+        
+        DownloadFile(Files.Version.Url, Files.Version.Path, Files.Version.Name)
+        local textPos = myHero.pos:To2D()
+        local NewVersion = tonumber(ReadFile(Files.Version.Path, Files.Version.Name))
+        if NewVersion > Version then
+            DownloadFile(Files.Lua.Url, Files.Lua.Path, Files.Lua.Name)
+            print("New PussyWarwick Version Press 2x F6")
+        else
+            print(Files.Version.Name .. ": No Updates Found")
+        end
+    
+    end
+    
+    AutoUpdate()
+
+end
 
 require "DamageLib"
 keybindings = { [ITEM_1] = HK_ITEM_1, [ITEM_2] = HK_ITEM_2, [ITEM_3] = HK_ITEM_3, [ITEM_4] = HK_ITEM_4, [ITEM_5] = HK_ITEM_5, [ITEM_6] = HK_ITEM_6}
@@ -26,7 +72,7 @@ function SetMovement(bool)
 end
 
 class "PussyWarwick"
-local Scriptname,Version,Author,LVersion = "Pussy-PussyWarwick","Puusykate"
+local Scriptname,Version,Author = "PussyWarwick", "Puusykate"
 
 function CurrentTarget(range)
 	if _G.SDK then
@@ -54,7 +100,7 @@ function PussyWarwick:__init()
 	else
 		orbwalkername = "Orbwalker not found"
 	end
-	PrintChat(Scriptname.." "..Version.." - Loaded...."..orbwalkername)
+	PrintChat(Scriptname.." "..Version.." - Loaded....")
 end
 
 function PussyWarwick:LoadSpells()
@@ -75,7 +121,7 @@ function PussyWarwick:LoadMenu()
 	self.Menu.ComboMode:MenuElement({id = "UseR", name = "R: Infinite Duress", value = true})
 	self.Menu.ComboMode:MenuElement({id = "comboActive", name = "Combo key", key = string.byte(" ")})
 	self.Menu.ComboMode:MenuElement({id = "UseHYDRA", name = "Use hydra", value = true})
-	self.Menu.ComboMode:MenuElement({id = "DrawDamage", name = "Draw damage on HPbar", value = true})
+	self.Menu.ComboMode:MenuElement({id = "DrawDamage", name = "Draw RRange", value = true})
 		
 	self.Menu:MenuElement({id = "HarassMode", name = "Harass", type = MENU})
 	self.Menu.HarassMode:MenuElement({id = "UseQ", name = "Q: Jaws of the Beast", value = true})
@@ -88,7 +134,8 @@ function PussyWarwick:LoadMenu()
 	self.Menu.ClearMode:MenuElement({id = "UseW", name = "W: Blood Hunt", value = true})
 	self.Menu.ClearMode:MenuElement({id = "UseE", name = "E: Primal Howl", value = true})
 	self.Menu.ClearMode:MenuElement({id = "clearActive", name = "Clear key", key = string.byte("V")})
-
+		
+	
 	self.Menu:MenuElement({id = "CustomSpellCast", name = "Use custom spellcast", tooltip = "Can fix some casting problems with wrong directions and so", value = true})
 	self.Menu:MenuElement({id = "delay", name = "Custom spellcast delay", value = 100, min = 0, max = 200, step = 5,tooltip = "increase this one if spells is going completely wrong direction", identifier = ""})
 	
@@ -102,21 +149,22 @@ function GetInventorySlotItem(itemID)
 		return nil
 	    end
 	
-	function UseHydra()
-		local HTarget = CurrentTarget(125)
+function UseHydra()
+		local HTarget = CurrentTarget(300)
 		if HTarget then 
-			local hydraitem = GetInventorySlotItem(3748) or GetInventorySlotItem(3077)
+			local hydraitem = GetInventorySlotItem(3748) or GetInventorySlotItem(3077) or GetInventorySlotItem(3074)
 			if hydraitem and myHero.attackData.state == STATE_WINDDOWN then
 				Control.CastSpell(keybindings[hydraitem],HTarget.pos)
                 Control.Attack(HTarget)
 			end
 		end
 	end
-    function UseHydraminion()
+   
+   function UseHydraminion()
         for i = 1, Game.MinionCount() do
 	    local minion = Game.Minion(i)
         if minion and minion.team == 300 or minion.team ~= myHero.team then 
-			local hydraitem = GetInventorySlotItem(3748) or GetInventorySlotItem(3077)
+			local hydraitem = GetInventorySlotItem(3748) or GetInventorySlotItem(3077) or GetInventorySlotItem(3074)
 			if hydraitem and myHero.attackData.state == STATE_WINDDOWN then
 				Control.CastSpell(keybindings[hydraitem])
                 Control.Attack(minion)
@@ -125,8 +173,8 @@ function GetInventorySlotItem(itemID)
         end
 	end
 
-    function UseBotrk()
-		local BTarget = CurrentTarget(400)
+function UseBotrk()
+		local BTarget = CurrentTarget(600)
 		if BTarget then 
 			local botrkitem = GetInventorySlotItem(3153) or GetInventorySlotItem(3144)
 			if botrkitem then
@@ -152,14 +200,17 @@ end
 
 function PussyWarwick:Tick()
 	if myHero.dead or Game.IsChatOpen() == true or IsRecalling() == true then return end
-	if self.Menu.HarassMode.harassActive:Value() and self:EnemyInRange(700) then
+	if self.Menu.HarassMode.harassActive:Value() then
 		self:Harass()
 	end
-	if self.Menu.ComboMode.comboActive:Value() and self:EnemyInRange(1400) then
+	if self.Menu.ComboMode.comboActive:Value() then
 		self:Combo()
 	end
 	if self.Menu.ClearMode.clearActive:Value() then
 		self:Jungle()
+	end
+	if self.Menu.ComboMode.DrawDamage:Value() then
+	self:Draw()
 	end
 end
 
@@ -199,18 +250,50 @@ function LeftClick(pos)
 	Control.mouse_event(MOUSEEVENTF_LEFTUP)
 	DelayAction(ReturnCursor,0.05,{pos})
 end
+
+function PussyWarwick:QDmg()
+	total = 0
+	local qLvl = myHero:GetSpellData(_Q).level
+    if qLvl > 0 then
+	local qdamage = 1.2 * myHero.totalDamage + 0.9 * myHero.ap + (({6, 6.5, 7, 7.5, 8})[qLvl] / 100  * target.maxHealth)
+	total = qdamage
+	end
+	return total
+
+end
+
+function PussyWarwick:RDmg()
+	total = 0
+	local rLvl = myHero:GetSpellData(_R).level
+    if rLvl > 0 then
+	local rdamage = (({175,350,525})[rLvl] + 1.67 * myHero.totalDamage)
+	total = rdamage
+	end
+	return total
+
+end
+
+function PussyWarwick:HpPred(unit, delay)
+	if _G.GOS then
+	hp =  GOS:HP_Pred(unit,delay)
+	else
+	hp = unit.health
+	end
+	return hp
+end
+
+
 function PussyWarwick:Draw()
     local textPos = myHero.pos:To2D()
-    if self:CanCast(_R) then Draw.Circle(myHero.pos,myHero.ms * 1.36 + 250, 3,  Draw.Color(255, 000, 222, 255)) end
+    if self:CanCast(_R) then Draw.Circle(myHero.pos, (2.5 * myHero.ms), Draw.Color(255, 000, 222, 255)) end
 	if self.Menu.ComboMode.DrawDamage:Value() then
 		for i, hero in pairs(self:GetEnemyHeroes()) do
 			local barPos = hero.hpBar
 			if not hero.dead and hero.pos2D.onScreen and barPos.onScreen and hero.visible then
-				local QDamage = (self:CanCast(_Q) and getdmg("Q",hero,myHero) or 0)
-				local WDamage = (self:CanCast(_W) and getdmg("W",hero,myHero) or 0)
-				local EDamage = (self:CanCast(_E) and getdmg("E",hero,myHero) or 0)
-				local damage = QDamage + WDamage + EDamage
-				if damage > hero.health then
+				local QDamage = (self:CanCast(_Q) and self:QDmg() or 0)
+				local RDamage = (self:CanCast(_R) and self:RDmg() or 0)
+				local damage = QDamage + RDamage
+				if damage > self:HpPred(hero,1) + hero.hpRegen * 1 then
 					Draw.Text("killable", 24, hero.pos2D.x, hero.pos2D.y,Draw.Color(0xFF00FF00))
 					
 				else
@@ -223,7 +306,7 @@ function PussyWarwick:Draw()
 		end	
 	end
 	if self.Menu.ComboMode.Key:Value() then
-		Draw.Text("Insta E: On", 20, textPos.x - 33, textPos.y + 50, Draw.Color(255, 000, 255, 000)) --above built in smite msg incase jungle PussyWarwick
+		Draw.Text("Insta E: On", 20, textPos.x - 33, textPos.y + 50, Draw.Color(255, 000, 255, 000)) 
 	else
 		Draw.Text("Insta E: Off", 20, textPos.x - 33, textPos.y + 50, Draw.Color(255, 225, 000, 000)) 
 	end
@@ -253,22 +336,23 @@ function PussyWarwick:CastSpell(spell,pos)
 	end
 end
 
+
 function PussyWarwick:Combo()
-    if self.Menu.ComboMode.UseHYDRA:Value() and not self:HasBuff(myHero, "Blood Hunt") and self:EnemyInRange(124) then
-        if myHero.attackData.state == STATE_WINDDOWN and not self:CanCast(_W) then
+    if self.Menu.ComboMode.UseHYDRA:Value() and self:HasBuff(myHero, "Blood Hunt") and self:EnemyInRange(300) then
+        if myHero.attackData.state == STATE_WINDDOWN then
             UseHydra()
         end
     end
-----------------------------Primal Howl
-    if self:CanCast(_E) and self:EnemyInRange(375) then 
+
+    if self:CanCast(_E) then 
 		local ETarget = CurrentTarget(375)
-		if self.Menu.ComboMode.UseE:Value() and self.Menu.ComboMode.Key:Value() == false and ETarget and not self:HasBuff(myHero, "Primal Howl") then
-			if self:EnemyInRange(125) and myHero.pos:DistanceTo(ETarget.pos) < 130 then
+		if self.Menu.ComboMode.UseE:Value() and self.Menu.ComboMode.Key:Value() == false and ETarget and self:HasBuff(myHero, "Primal Howl") then
+			if self:EnemyInRange(375) and myHero.pos:DistanceTo(ETarget.pos) < 375 then
 				Control.CastSpell(HK_E)
 			end
 		end
         if self.Menu.ComboMode.UseE:Value() and self.Menu.ComboMode.Key:Value() == true and ETarget and not self:HasBuff(myHero, "Primal Howl") then
-			if self:EnemyInRange(375) and self:CanCast(_Q) and myHero.pos:DistanceTo(ETarget.pos) < 375 then
+			if self:EnemyInRange(375) and self:CanCast(_E) and myHero.pos:DistanceTo(ETarget.pos) < 375 then
 				Control.CastSpell(HK_E)
 			end
 		end
@@ -277,27 +361,27 @@ function PussyWarwick:Combo()
 	if self:CanCast(_Q) and self:EnemyInRange(350) then 
 		local QTarget = CurrentTarget(350)
 		if self.Menu.ComboMode.UseQ:Value() and QTarget then
-            if self:EnemyInRange(350) and myHero.pos:DistanceTo(QTarget.pos) > 350 then
+            if self:EnemyInRange(350) and myHero.pos:DistanceTo(QTarget.pos) < 350 and myHero.pos:DistanceTo(QTarget.pos) > 125 then
 				Control.CastSpell(HK_Q, QTarget)
             end
 		end
 	end
 
-    if self:CanCast(_R) and self:EnemyInRange(250) then 
-        local rRange = (myHero.ms * 1.36)
+    if self:CanCast(_R) then 
+        local rRange = 2.5 * myHero.ms
 		local RTarget = CurrentTarget(rRange)
         if self.Menu.ComboMode.UseR:Value() and RTarget then
-			if self:EnemyInRange(rRange) and myHero.pos:DistanceTo(RTarget.pos) < rRange + 250 then
-				Control.CastSpell(HK_R, RTarget.pos)
+			if self:EnemyInRange(rRange) and myHero.pos:DistanceTo(RTarget.pos) < rRange then
+				Control.CastSpell(HK_R, RTarget)
 			end	
         end
     end
 	
 
-    if self:EnemyInRange(400) and not self:CanCast(_Q) then 
-        local BTarget = CurrentTarget(400)
+    if self:EnemyInRange(600) and not self:CanCast(_Q) then 
+        local BTarget = CurrentTarget(600)
         if BTarget then
-            if myHero.pos:DistanceTo(BTarget.pos) < 400 then
+            if myHero.pos:DistanceTo(BTarget.pos) < 600 then
 			    UseBotrk()
             end
         end
@@ -328,38 +412,38 @@ function PussyWarwick:EnemyInRange(range)
 end
 
 function PussyWarwick:Harass()
-    if self.Menu.ComboMode.UseHYDRA:Value() and not self:HasBuff(myHero, "Blood Hunt") and self:EnemyInRange(124) then
-        if myHero.attackData.state == STATE_WINDDOWN and not self:CanCast(_W) then
+    if self.Menu.ComboMode.UseHYDRA:Value() and self:HasBuff(myHero, "Blood Hunt") and self:EnemyInRange(300) then
+        if myHero.attackData.state == STATE_WINDDOWN then
             UseHydra()
         end
     end
-    if self:CanCast(_E) and self:EnemyInRange(720) then 
-		local ETarget = CurrentTarget(720)
-		if self.Menu.HarassMode.UseE:Value() and self.Menu.ComboMode.Key:Value() == false and ETarget and not self:HasBuff(myHero, "PussyWarwickCounterStrike") then
-			if self:EnemyInRange(125) and myHero.pos:DistanceTo(ETarget.pos) < 130 then
+    if self:CanCast(_E) then 
+		local ETarget = CurrentTarget(375)
+		if self.Menu.HarassMode.UseE:Value() and self.Menu.ComboMode.Key:Value() == false and ETarget and self:HasBuff(myHero, "Primal Howl") then
+			if self:EnemyInRange(375) and myHero.pos:DistanceTo(ETarget.pos) < 375 then
 				Control.CastSpell(HK_E)
 			end
 		end
-        if self.Menu.HarassMode.UseE:Value() and self.Menu.ComboMode.Key:Value() == true and ETarget and not self:HasBuff(myHero, "PussyWarwickCounterStrike") then
-			if self:EnemyInRange(720) and self:CanCast(_Q) and myHero.pos:DistanceTo(ETarget.pos) < 720 then
+        if self.Menu.HarassMode.UseE:Value() and self.Menu.ComboMode.Key:Value() == true and ETarget and not self:HasBuff(myHero, "Primal Howl") then
+			if self:EnemyInRange(375) and self:CanCast(_E) and myHero.pos:DistanceTo(ETarget.pos) < 375 then
 				Control.CastSpell(HK_E)
 			end
 		end
 	end
 
-	if self:CanCast(_Q) and self:EnemyInRange(700) then 
-		local QTarget = CurrentTarget(700)
+	if self:CanCast(_Q) then 
+		local QTarget = CurrentTarget(350)
 		if self.Menu.HarassMode.UseQ:Value() and QTarget then
-            if self:EnemyInRange(700) and myHero.pos:DistanceTo(QTarget.pos) > 250 then
+            if self:EnemyInRange(350) and myHero.pos:DistanceTo(QTarget.pos) < 350 and myHero.pos:DistanceTo(QTarget.pos) > 125 then
 				Control.CastSpell(HK_Q, QTarget)
             end
 		end
 	end
 
-	if self:CanCast(_W) and self:EnemyInRange(175) then 
-		local WTarget = CurrentTarget(175)
+	if self:CanCast(_W) then 
+		local WTarget = CurrentTarget(125)
 		if self.Menu.HarassMode.UseW:Value() and WTarget then
-			if self:EnemyInRange(175) and myHero.attackData.state == STATE_WINDDOWN then
+			if self:EnemyInRange(125) and myHero.attackData.state == STATE_WINDDOWN then
 				Control.CastSpell(HK_W)
                 Control.Attack(WTarget)
 			end
@@ -373,25 +457,26 @@ function PussyWarwick:Jungle()
 	local minion = Game.Minion(i)
     if minion and minion.team == 300 or minion.team ~= myHero.team then
     if self:CanCast(_E) and minion then 
-		if self.Menu.ClearMode.UseE:Value() and self.Menu.ComboMode.Key:Value()  == false and not self:HasBuff(myHero, "PussyWarwickCounterStrike") then
-			if myHero.pos:DistanceTo(minion.pos) < 175 then
+		if self.Menu.ClearMode.UseE:Value() and self.Menu.ComboMode.Key:Value()  == false and self:HasBuff(myHero, "Primal Howl") then
+			if myHero.pos:DistanceTo(minion.pos) < 375 then
 				Control.CastSpell(HK_E)
 			end
 		end
-        if self.Menu.ClearMode.UseE:Value() and self.Menu.ComboMode.Key:Value()  == true and not self:HasBuff(myHero, "PussyWarwickCounterStrike") then
-			if myHero.pos:DistanceTo(minion.pos) < 700 and self:CanCast(_Q) then
+        if self.Menu.ClearMode.UseE:Value() and self.Menu.ComboMode.Key:Value()  == true and not self:HasBuff(myHero, "Primal Howl") then
+			if myHero.pos:DistanceTo(minion.pos) < 375 and self:CanCast(_E) then
 				Control.CastSpell(HK_E)
 			end
 		end
-	end
+	end	
+
     if self.Menu.ComboMode.UseHYDRA:Value() and not self:HasBuff(myHero, "Blood Hunt") and minion then
-        if myHero.attackData.state == STATE_WINDDOWN and not self:CanCast(_W) and myHero.pos:DistanceTo(minion.pos) < 150 then
+        if myHero.attackData.state == STATE_WINDDOWN and not self:CanCast(_W) and myHero.pos:DistanceTo(minion.pos) < 300 then
             UseHydraminion()
         end
     end
 	if self:CanCast(_Q) and minion then 
-		if self.Menu.ClearMode.UseQ:Value() and ValidTarget(minion, 700) then
-            if myHero.pos:DistanceTo(minion.pos) > 250 then
+		if self.Menu.ClearMode.UseQ:Value() and ValidTarget(minion, 350) then
+            if myHero.pos:DistanceTo(minion.pos) < 350 and myHero.pos:DistanceTo(minion.pos) > 125 then
 				Control.CastSpell(HK_Q, minion)
             end
 		end
