2a12388ec2b1bf35a18a4942ee0d2e4d43ae06d0
Pussykate <35004487+Pussykate@users.noreply.github.com>
2018-11-18 13:45:56 +0100

    Update PussyCassio.lua

--- PussyCassio.lua
+++ PussyCassio.lua
@@ -1,4 +1,51 @@
+-- [ AutoUpdate ]
+do
+    
+    local Version = 0.01
+    
+    local Files = {
+        Lua = {
+            Path = SCRIPT_PATH,
+            Name = "PussyCassio.lua",
+            Url = "https://raw.githubusercontent.com/Pussykate/GoS/master/PussyCassio.lua"
+        },
+        Version = {
+            Path = SCRIPT_PATH,
+            Name = "PussyCassio.version",
+            Url = "https://raw.githubusercontent.com/Pussykate/GoS/master/PussyCassio.version"
+        }
+    }
+    
+    local function AutoUpdate()
+        
+        local function DownloadFile(url, path, fileName)
+            DownloadFileAsync(url, path .. fileName, function() end)
+            while not FileExist(path .. fileName) do end
+        end
+        
+        local function ReadFile(path, fileName)
+            local file = io.open(path .. fileName, "r")
+            local result = file:read()
+            file:close()
+            return result
+        end
+        
+        DownloadFile(Files.Version.Url, Files.Version.Path, Files.Version.Name)
+        local textPos = myHero.pos:To2D()
+        local NewVersion = tonumber(ReadFile(Files.Version.Path, Files.Version.Name))
+        if NewVersion > Version then
+            DownloadFile(Files.Lua.Url, Files.Lua.Path, Files.Lua.Name)
+            print("New PussyTristana Version Press 2x F6")
+        else
+            print(Files.Version.Name .. ": No Updates Found")
+        end
+    
+    end
+    
+    AutoUpdate()
 
+end
+	
 	local Orb = 3
 	local TEAM_ALLY = myHero.team
 	local TEAM_JUNGLE = 300
