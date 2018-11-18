26e780bcce8891176d875f8164f2b9d213eca52f
Pussykate <35004487+Pussykate@users.noreply.github.com>
2018-11-18 13:49:13 +0100

    Update PussyCassio.lua

--- PussyCassio.lua
+++ PussyCassio.lua
@@ -35,7 +35,7 @@ do
         local NewVersion = tonumber(ReadFile(Files.Version.Path, Files.Version.Name))
         if NewVersion > Version then
             DownloadFile(Files.Lua.Url, Files.Lua.Path, Files.Lua.Name)
-            print("New PussyTristana Version Press 2x F6")
+            print("New PussyCassio Version Press 2x F6")
         else
             print(Files.Version.Name .. ": No Updates Found")
         end
