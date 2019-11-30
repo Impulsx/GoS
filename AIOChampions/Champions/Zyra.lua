8750cbaafec82f024263f6747c5bcbd07b7379b2
Pussykate <35004487+Pussykate@users.noreply.github.com>
2019-11-30 16:12:48 +0100

    Update Zyra.lua

--- AIOChampions/Champions/Zyra.lua
+++ AIOChampions/Champions/Zyra.lua
@@ -1,6 +1,60 @@
+function GetDistanceSqr(p1, p2)
+	if not p1 then return math.huge end
+	p2 = p2 or myHero
+	local dx = p1.x - p2.x
+	local dz = (p1.z or p1.y) - (p2.z or p2.y)
+	return dx*dx + dz*dz
+end
+
+function GetDistance(p1, p2)
+	p2 = p2 or myHero
+	return math.sqrt(GetDistanceSqr(p1, p2))
+end
+
+function GetDistance2D(p1,p2)
+	return math.sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y))
+end
+
+function IsImmobileTarget(unit)
+	for i = 0, unit.buffCount do
+		local buff = unit:GetBuff(i)
+		if buff and (buff.type == 5 or buff.type == 11 or buff.type == 29 or buff.type == 24 or buff.name == 10 ) and buff.count > 0 then
+			return true
+		end
+	end
+	return false	
+end
+
+function GetImmobileCount(range, pos)
+    local pos = pos.pos
+	local count = 0
+	for i = 1, Game.HeroCount() do 
+	local hero = Game.Hero(i)
+	local Range = range * range
+		if hero.isEnemy and GetDistanceSqr(pos, hero.pos) < Range and IsImmobileTarget(hero) then
+		count = count + 1
+		end
+	end
+	return count
+end
+
+function GetEnemyCount(range, pos)
+    local pos = pos.pos
+	local count = 0
+	for i = 1, Game.HeroCount() do 
+	local hero = Game.Hero(i)
+	local Range = range * range
+		if hero.team ~= TEAM_ALLY and GetDistanceSqr(pos, hero.pos) < Range and IsValid(hero) then
+		count = count + 1
+		end
+	end
+	return count
+end
+
 function LoadScript()
-	Menu = MenuElement({type = MENU, id = myHero.networkID, name = myHero.charName})
 	
+	Menu = MenuElement({type = MENU, id = myHero.networkID, name = myHero.charName})
+	Menu:MenuElement({name = " ", drop = {"Version 0.01"}})	
 	--AutoE
 	Menu:MenuElement({type = MENU, id = "AutoE", name = "AutoE"})
 	Menu.AutoE:MenuElement({id = "UseE", name = "Auto[E]on Immobile", value = true})	
@@ -82,8 +136,28 @@ function LoadScript()
 		Orb = 4
 	end	
 	Callback.Add("Tick", function() Tick() end)
-	Callback.Add("Draw", function() Draw() end)		
+	
+	Callback.Add("Draw", function()
+		if Menu.Drawing.DrawR:Value() and Ready(_R) then
+		Draw.Circle(myHero, 700, 1, Draw.Color(255, 225, 255, 10))
+		end                                                 
+		if Menu.Drawing.DrawQ:Value() and Ready(_Q) then
+		Draw.Circle(myHero, 800, 1, Draw.Color(225, 225, 0, 10))
+		end
+		if Menu.Drawing.DrawE:Value() and Ready(_E) then
+		Draw.Circle(myHero, 1100, 1, Draw.Color(225, 225, 125, 10))
+		end
+		if Menu.Drawing.DrawW:Value() and Ready(_W) then
+		Draw.Circle(myHero, 850, 1, Draw.Color(225, 225, 125, 10))
+		end
+		local textPos = myHero.pos:To2D()	
+		if not FileExist(COMMON_PATH .. "GamsteronPrediction.lua") then
+			Draw.Text("GsoPred. installed Press 2x F6", 50, textPos.x + 100, textPos.y - 250, Draw.Color(255, 255, 0, 0))
+		end
+	end)	
+	
 end
+
 
 function Tick()
 if MyHeroNotReady() then return end
@@ -102,19 +176,16 @@ local Mode = GetMode()
 	AutoE()
 	AutoR()
 	ImmoR()	
-	UseW()
+	UseW()	
+		
 end
 
 function UseW()
 local target = GetTarget(1200)     	
 if target == nil then return end		
-	if IsValid(target) and myHero.pos:DistanceTo(target.pos) <= 850 and Ready(_W) then
+	if IsValid(target) and myHero.pos:DistanceTo(target.pos) <= 850 and Ready(_W) and myHero:GetSpellData(_W).ammo > 0 then
 		if IsImmobileTarget(target) then   
 			Control.CastSpell(HK_W, target.pos) 
-			DelayAction(function() 
-			Control.CastSpell(HK_W, target.pos)
-		
-			end, 0.05)
 		end	
 	end
 end
@@ -159,27 +230,6 @@ if target == nil then return end
 			Control.CastSpell(HK_R, pred.CastPosition)
 		end	
 	end
-end
-
-			
-function Draw()
-  if myHero.dead then return end
-	if Menu.Drawing.DrawR:Value() and Ready(_R) then
-    Draw.Circle(myHero, 700, 1, Draw.Color(255, 225, 255, 10))
-	end                                                 
-	if Menu.Drawing.DrawQ:Value() and Ready(_Q) then
-    Draw.Circle(myHero, 800, 1, Draw.Color(225, 225, 0, 10))
-	end
-	if Menu.Drawing.DrawE:Value() and Ready(_E) then
-    Draw.Circle(myHero, 1100, 1, Draw.Color(225, 225, 125, 10))
-	end
-	if Menu.Drawing.DrawW:Value() and Ready(_W) then
-    Draw.Circle(myHero, 850, 1, Draw.Color(225, 225, 125, 10))
-	end
-	local textPos = myHero.pos:To2D()	
-	if not FileExist(COMMON_PATH .. "GamsteronPrediction.lua") then
-		Draw.Text("GsoPred. installed Press 2x F6", 50, textPos.x + 100, textPos.y - 250, Draw.Color(255, 255, 0, 0))
-	end	
 end
        
 function KillSteal()	
@@ -227,11 +277,8 @@ local target = GetTarget(1200)
 if target == nil then return end
 	if IsValid(target) then
 
-		if myHero.pos:DistanceTo(target.pos) <= 850 and Menu.Combo.UseW:Value() and Ready(_W) then
+		if myHero.pos:DistanceTo(target.pos) <= 850 and Menu.Combo.UseW:Value() and Ready(_W) and myHero:GetSpellData(_W).ammo > 0 then
 			Control.CastSpell(HK_W, target.pos)
-			DelayAction(function() 
-			Control.CastSpell(HK_W, target.pos) 
-			end, 0.05)
 		end			
 		
 		if myHero.pos:DistanceTo(target.pos) <= 800 and Menu.Combo.UseQ:Value() and Ready(_Q) then
@@ -283,7 +330,7 @@ function Clear()
 	for i = 1, Game.MinionCount() do
     local minion = Game.Minion(i)
 
-		if minion.team == TEAM_ENEMY and IsValid(minion, 1200) and myHero.mana/myHero.maxMana >= Menu.Clear.Mana:Value() / 100 then					
+		if minion.team == TEAM_ENEMY and IsValid(minion) and myHero.mana/myHero.maxMana >= Menu.Clear.Mana:Value() / 100 then					
 			
 			if myHero.pos:DistanceTo(minion.pos) <= 800 and Ready(_Q) and Menu.Clear.UseQ:Value() then
 				Control.CastSpell(HK_Q, minion.pos)
@@ -300,7 +347,7 @@ function JungleClear()
 	for i = 1, Game.MinionCount() do
     local minion = Game.Minion(i)	
 
-		if minion.team == TEAM_JUNGLE and IsValid(minion, 1200) and myHero.mana/myHero.maxMana >= Menu.JClear.Mana:Value() / 100 then	
+		if minion.team == TEAM_JUNGLE and IsValid(minion) and myHero.mana/myHero.maxMana >= Menu.JClear.Mana:Value() / 100 then	
 			if myHero.pos:DistanceTo(minion.pos) <= 800 and Ready(_Q) and Menu.JClear.UseQ:Value() then
 				Control.CastSpell(HK_Q, minion.pos)
 			end
