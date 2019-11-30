144cfca721a0c2fc077405ec08e290ff68435a2f
Pussykate <35004487+Pussykate@users.noreply.github.com>
2019-11-30 11:22:06 +0100

    Update newVersion.lua

--- AIOChampions/newVersion.lua
+++ AIOChampions/newVersion.lua
@@ -23,7 +23,7 @@ Data = {
         },
         Lux = {
             Changelog = "Lux Changelog Here",
-            Version = 0.02,
+            Version = 0.03,
         },
         Soraka = {
             Changelog = "Soraka Changelog Here",
