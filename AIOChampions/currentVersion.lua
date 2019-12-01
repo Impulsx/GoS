1f8b6634c8c81639365f3060dc6b41e30c531a62
Pussykate <35004487+Pussykate@users.noreply.github.com>
2019-12-01 12:19:35 +0100

    Update currentVersion.lua

--- AIOChampions/currentVersion.lua
+++ AIOChampions/currentVersion.lua
@@ -35,7 +35,7 @@ Data = {
         },
         Nidalee = {
             Changelog = "Nidalee Changelog Here",
-            Version = 0.01,
+            Version = 0.02,
         },        
     },
 }
