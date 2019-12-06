c3a1b67616f59d47f07b6c801cca02e186450955
Pussykate <35004487+Pussykate@users.noreply.github.com>
2019-12-06 13:04:53 +0100

    Update XinZhao.lua

--- AIOChampions/Champions/XinZhao.lua
+++ AIOChampions/Champions/XinZhao.lua
@@ -11,11 +11,26 @@ function GetMinionCount(range, pos)
 	return count
 end
 
+function IsImmobileTarget(unit)
+	for i = 0, unit.buffCount do
+		local buff = unit:GetBuff(i)
+		if buff and (buff.type == 5 or buff.type == 11 or buff.type == 29 or buff.type == 24 or buff.name == 10 ) and buff.count > 0 then
+			return true
+		end
+	end
+	return false	
+end
+
 function LoadScript()
 	
 	Menu = MenuElement({type = MENU, id = myHero.networkID, name = myHero.charName})
-	Menu:MenuElement({name = " ", drop = {"Version 0.01"}})	
+	Menu:MenuElement({name = " ", drop = {"Version 0.02"}})	
 	Menu:MenuElement({type = MENU, id = "Mode", name = myHero.charName})
+	
+	--Main Menu-- PussyXinZhao -- AutoW
+	Menu.Mode:MenuElement({type = MENU, id = "Auto", name = "AutoW"})
+	Menu.Mode.Auto:MenuElement({id = "W", name = "[W] Immobile Target", value = true})
+	
 	
 	--Main Menu-- PussyXinZhao -- Combo
 	Menu.Mode:MenuElement({type = MENU, id = "Combo", name = "Combo"})
@@ -95,8 +110,19 @@ local Mode = GetMode()
 	elseif Mode == "Clear" then
 		Clear()		
 	end	
-		
-	KS()		
+	AutoW()	
+	KS()
+end
+
+function AutoW()
+local target = GetTarget(1000)     	
+if target == nil then return end
+	
+	if IsValid(target) and myHero.pos:DistanceTo(target.pos) <= 900 and IsImmobileTarget(target) then
+		if Menu.Mode.Auto.W:Value() and Ready(_W) and not myHero.isChanneling then
+			Control.CastSpell(HK_W, target.pos)
+		end
+	end			
 end
 
 function KS()
