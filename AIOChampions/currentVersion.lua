b20b61d19005fc6258fe73e481c5bb4244b62d85
Pussykate <35004487+Pussykate@users.noreply.github.com>
2019-12-14 10:24:26 +0100

    Update currentVersion.lua

--- AIOChampions/currentVersion.lua
+++ AIOChampions/currentVersion.lua
@@ -39,7 +39,7 @@ Data = {
         }, 
         Ryze = {
             Changelog = "Ryze Changelog Here",
-            Version = 0.02,
+            Version = 0.03,
         },
         Neeko = {
             Changelog = "Neeko Changelog Here",
