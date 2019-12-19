9705d56cb82e7e488a411f299e42c3c59b885831
Pussykate <35004487+Pussykate@users.noreply.github.com>
2019-12-19 16:29:35 +0100

    Update newVersion.lua

--- AIOChampions/newVersion.lua
+++ AIOChampions/newVersion.lua
@@ -63,7 +63,7 @@ Data = {
         }, 
         Morgana = {
             Changelog = "Morgana Changelog Here",
-            Version = 0.01,
+            Version = 0.02,
         },         
     },
 }
