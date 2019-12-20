e05920f6a5d65309b5df7a0a240a00c257ce1bc9
Pussykate <35004487+Pussykate@users.noreply.github.com>
2019-12-20 11:14:49 +0100

    Update currentVersion.lua

--- AIOChampions/currentVersion.lua
+++ AIOChampions/currentVersion.lua
@@ -67,7 +67,7 @@ Data = {
         },
         Kassadin = {
             Changelog = "Kassadin Changelog Here",
-            Version = 0.01,
+            Version = 0.02,
         },        
     },
 }
