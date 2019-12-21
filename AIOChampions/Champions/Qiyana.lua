6c59ee9b69bf302416eb85032d94f30da654ac72
Pussykate <35004487+Pussykate@users.noreply.github.com>
2019-12-21 12:55:16 +0100

    Update Qiyana.lua

--- AIOChampions/Champions/Qiyana.lua
+++ AIOChampions/Champions/Qiyana.lua
@@ -6,47 +6,39 @@ function HasBuff(unit, buffname)
 		end
 	end
 	return false
-end
+end			
 
-function GotBuff(unit, buffname)
-  for i = 0, unit.buffCount do
-    local buff = unit:GetBuff(i)
-    if buff.name == buffname and buff.count > 0 then 
-      return buff.count
+function CheckWall(from, to, distance)
+    local pos1 = to + (to - from):Normalized() * 50
+    local pos2 = pos1 + (to - from):Normalized() * (distance - 50)
+    local point1 = Point(pos1.x, pos1.z)
+    local point2 = Point(pos2.x, pos2.z)
+    if MapPosition:intersectsWall(LineSegment(point1, point2)) or (MapPosition:inWall(point1) and MapPosition:inWall(point2)) then
+        return true
     end
-  end
-  return 0
+    return false
 end
 
-function GetMinionCount(range, pos)
-    local pos = pos.pos
-	local count = 0
-	for i = 1,Game.MinionCount() do
-	local hero = Game.Minion(i)
-	local Range = range * range
-		if hero.team ~= TEAM_ALLY and hero.dead == false and GetDistanceSqr(pos, hero.pos) < Range then
-		count = count + 1
-		end
-	end
-	return count
-end
+require "MapPositionGOS"
 
 function LoadScript() 	 
 	
 	Menu = MenuElement({type = MENU, id = myHero.networkID, name = myHero.charName})
-	Menu:MenuElement({name = " ", drop = {"Version 0.01"}})
+	Menu:MenuElement({name = " ", drop = {"Version 0.02"}})
 	
 	--ComboMenu  
 	Menu:MenuElement({type = MENU, id = "Combo", name = "Combo"})
-	Menu.Combo:MenuElement({id = "UseQ", name = "[Q]", value = true})		
+	Menu.Combo:MenuElement({id = "UseQ", name = "[Q]", value = true})
+	Menu.Combo:MenuElement({id = "UseQW", name = "[Q]only if Ready[W]", value = true})	
 	Menu.Combo:MenuElement({id = "UseW", name = "[W]", value = true})
 	Menu.Combo:MenuElement({id = "UseE", name = "[E]", value = true})		
-	Menu.Combo:MenuElement({id = "UseR", name = "[R]", value = true})
+	Menu.Combo:MenuElement({id = "UseR", name = "[R] Check Wall.pos", value = true})
 			
 
 	--HarassMenu
 	Menu:MenuElement({type = MENU, id = "Harass", name = "Harass"})	
 	Menu.Harass:MenuElement({id = "UseQ", name = "[Q]", value = true})
+	Menu.Harass:MenuElement({id = "UseQW", name = "[Q]only if Ready[W]", value = true})	
 	Menu.Harass:MenuElement({id = "UseW", name = "[W]", value = true})		
 	Menu.Harass:MenuElement({id = "Mana", name = "Min Mana to Harass", value = 40, min = 0, max = 100, identifier = "%"})
   
@@ -62,25 +54,18 @@ function LoadScript()
 	Menu.JClear:MenuElement({id = "UseQ", name = "[Q]", value = true})         	
 	Menu.JClear:MenuElement({id = "UseW", name = "[W]", value = true})
 	Menu.JClear:MenuElement({id = "UseE", name = "[E]", value = true})
-	Menu.JClear:MenuElement({id = "Mana", name = "Min Mana to JungleClear", value = 40, min = 0, max = 100, identifier = "%"}) 
-
-	--LastHit
-	Menu:MenuElement({type = MENU, id = "Last", name = "LastHit Minion"})
-	Menu.Last:MenuElement({id = "UseW", name = "[W]", value = true})
-	Menu.Last:MenuElement({id = "UseQ", name = "[Q]", value = true})	
-	Menu.Last:MenuElement({id = "Mana", name = "Min Mana to LastHit", value = 20, min = 0, max = 100, identifier = "%"})
-	Menu.Last:MenuElement({id = "Active", name = "LastHit Key", key = string.byte("X")})	
+	Menu.JClear:MenuElement({id = "Mana", name = "Min Mana to JungleClear", value = 40, min = 0, max = 100, identifier = "%"}) 	
  
 	--KillSteal
 	Menu:MenuElement({type = MENU, id = "ks", name = "KillSteal"})
 	Menu.ks:MenuElement({id = "UseQ", name = "[Q]", value = true})	
-	Menu.ks:MenuElement({id = "UseRW", name = "[R]+[W] if in range", value = true})	
-	Menu.ks:MenuElement({id = "UseRQ", name = "[R]+[Q] if range bigger", value = true})	
-
+	Menu.ks:MenuElement({id = "UseQ2", name = "[Q] Terrain Buff", value = true})	
+	Menu.ks:MenuElement({id = "UseE", name = "[E]", value = true})	
+--[[
 	--Prediction
 	Menu:MenuElement({type = MENU, id = "Pred", name = "Prediction"})	
 	Menu.Pred:MenuElement({id = "PredE", name = "Hitchance[E]", value = 1, drop = {"Normal", "High", "Immobile"}})
- 
+]] 
 	--Drawing 
 	Menu:MenuElement({type = MENU, id = "Drawing", name = "Drawings"})
 	Menu.Drawing:MenuElement({id = "DrawQ", name = "Draw [Q] Range", value = false})
@@ -102,10 +87,10 @@ function LoadScript()
 	Callback.Add("Tick", function() Tick() end)
 	
 	Callback.Add("Draw", function()
-		local textPos = myHero.pos:To2D()	
-		if not FileExist(COMMON_PATH .. "GamsteronPrediction.lua") then
-			Draw.Text("GsoPred. installed Press 2x F6", 50, textPos.x + 100, textPos.y - 250, Draw.Color(255, 255, 0, 0))
-		end  
+		--local textPos = myHero.pos:To2D()	
+		--if not FileExist(COMMON_PATH .. "GamsteronPrediction.lua") then
+			--Draw.Text("GsoPred. installed Press 2x F6", 50, textPos.x + 100, textPos.y - 250, Draw.Color(255, 255, 0, 0))
+		--end  
 		
 		if myHero.dead then return end
 		
@@ -130,41 +115,55 @@ if MyHeroNotReady() then return end
 local Mode = GetMode()
 	if Mode == "Combo" then
 		Combo()
+		CastUlt()
 	elseif Mode == "Harass" then
 		Harass()
 	elseif Mode == "Clear" then
 		Clear()
 		JungleClear()
-	elseif Mode == "LastHit" then
-		if Menu.Last.Active:Value() then
-			--LastHit()	
-		end
 			
 	end	
 
-	--KillSteal()
+	KillSteal()
 end
+
+function CastUlt()
+    for i = 1, Game.HeroCount() do
+        local hero = Game.Hero(i)
+		if myHero.pos:DistanceTo(hero.pos) < 875 and hero.team == TEAM_ENEMY and IsValid(hero) then
+			if Menu.Combo.UseR:Value() and Ready(_R) and CheckWall(myHero.pos,  hero:GetPrediction(0.31, 2000), 280) then
+				Control.CastSpell(HK_R, hero.pos)
+			end
+		end
+	end
+end	
 
 function Combo()
 local target = GetTarget(1200)
 if target == nil then return end
-	if IsValid(target) then
-   
-		if myHero.pos:DistanceTo(target.pos) < 875 and Ready(_R) then
-			Control.CastSpell(HK_R, target.pos)
-        end			
+	if IsValid(target) then		
 		
 		if Menu.Combo.UseE:Value() and myHero.pos:DistanceTo(target.pos) < 650 and Ready(_E) then			
 			Control.CastSpell(HK_E, target)
         end		
 		
-		if Menu.Combo.UseQ:Value() and myHero.pos:DistanceTo(target.pos) < 650 and Ready(_Q) then
-			Control.CastSpell(HK_Q, target.pos)
+		if Menu.Combo.UseQW:Value() then 
+			if myHero.pos:DistanceTo(target.pos) < 650 and Ready(_Q) and Ready(_W) and not HasBuff(myHero, "qiyanawenchantedbuff") then
+				Control.CastSpell(HK_Q, target.pos)
+			end
+		else
+			if myHero.pos:DistanceTo(target.pos) < 650 and Menu.Combo.UseQ:Value() and Ready(_Q) and not HasBuff(myHero, "qiyanawenchantedbuff") then
+				Control.CastSpell(HK_Q, target.pos)
+			end			
         end
        
 		if Menu.Combo.UseW:Value() and myHero.pos:DistanceTo(target.pos) < 1100 and Ready(_W) then
 			Control.CastSpell(HK_W, target.pos)	
-        end				
+        end	
+
+		if HasBuff(myHero, "qiyanawenchantedbuff") and myHero.pos:DistanceTo(target.pos) < 710 then
+			Control.CastSpell(HK_Q, target.pos)
+		end	
 	end
 end
 
@@ -174,13 +173,23 @@ if target == nil then return end
 	if IsValid(target) then
         local mana_ok = myHero.mana/myHero.maxMana >= Menu.Harass.Mana:Value() / 100
         
-		if Menu.Harass.UseQ:Value() and mana_ok and myHero.pos:DistanceTo(target.pos) < 650 and Ready(_Q) then
-			Control.CastSpell(HK_Q, target.pos)
+		if Menu.Harass.UseQW:Value() then 
+			if myHero.pos:DistanceTo(target.pos) < 650 and Ready(_Q) and Ready(_W) and not HasBuff(myHero, "qiyanawenchantedbuff") then
+				Control.CastSpell(HK_Q, target.pos)
+			end
+		else
+			if myHero.pos:DistanceTo(target.pos) < 650 and Menu.Harass.UseQ:Value() and Ready(_Q) and not HasBuff(myHero, "qiyanawenchantedbuff") then
+				Control.CastSpell(HK_Q, target.pos)
+			end			
         end
 		
         if Menu.Harass.UseW:Value() and mana_ok and myHero.pos:DistanceTo(target.pos) < 710 and Ready(_W) then			
 			Control.CastSpell(HK_W, target.pos)
-        end				
+        end	
+
+		if HasBuff(myHero, "qiyanawenchantedbuff") and myHero.pos:DistanceTo(target.pos) < 710 then
+			Control.CastSpell(HK_Q, target.pos)
+		end			
 	end
 end		
 
@@ -191,16 +200,20 @@ function Clear()
             local mana_ok = myHero.mana/myHero.maxMana >= Menu.Clear.Mana:Value() / 100
             
             if Menu.Clear.UseE:Value() and mana_ok and myHero.pos:DistanceTo(minion.pos) < 650 and Ready(_E) then
-				Control.CastSpell(HK_E, minion.pos)
+				Control.CastSpell(HK_E, minion)
             end			
 			
-			if Menu.Clear.UseQ:Value() and mana_ok and myHero.pos:DistanceTo(minion.pos) < 650 and Ready(_Q) then
+			if Menu.Clear.UseQ:Value() and mana_ok and myHero.pos:DistanceTo(minion.pos) < 650 and Ready(_Q) and Ready(_W) then
 				Control.CastSpell(HK_Q, minion,pos)	
             end
 			
             if Menu.Clear.UseW:Value() and mana_ok and myHero.pos:DistanceTo(minion.pos) < 1100 and Ready(_W) then	
 				Control.CastSpell(HK_W, minion.pos)	
-            end			
+            end
+
+			if HasBuff(myHero, "qiyanawenchantedbuff") and myHero.pos:DistanceTo(minion.pos) < 710 then
+				Control.CastSpell(HK_Q, minion.pos)
+			end				
         end
     end
 end
@@ -212,39 +225,20 @@ function JungleClear()
             local mana_ok = myHero.mana/myHero.maxMana >= Menu.JClear.Mana:Value() / 100
             
             if Menu.JClear.UseE:Value() and mana_ok and myHero.pos:DistanceTo(minion.pos) < 650 and Ready(_E) then
-				Control.CastSpell(HK_E, minion.pos)
+				Control.CastSpell(HK_E, minion)
             end				
 			
-			if Menu.JClear.UseQ:Value() and mana_ok and myHero.pos:DistanceTo(minion.pos) < 650 and Ready(_Q) then
+			if Menu.JClear.UseQ:Value() and mana_ok and myHero.pos:DistanceTo(minion.pos) < 650 and Ready(_Q) and Ready(_W) then
                 Control.CastSpell(HK_Q, minion.pos)
             end
 			
             if Menu.JClear.UseW:Value() and mana_ok and myHero.pos:DistanceTo(minion.pos) < 1100 and Ready(_W) then	
 				Control.CastSpell(HK_W, minion.pos)	
-            end		
-        end
-    end
-end
---[[
-function LastHit()
-    for i = 1, Game.MinionCount() do
-    local minion = Game.Minion(i)
-        if minion.team == TEAM_ENEMY then
-            local mana_ok = myHero.mana/myHero.maxMana >= Menu.Last.Mana:Value() / 100
-            
-			if Menu.Last.UseQ:Value() and mana_ok and myHero.pos:DistanceTo(minion.pos) < 650 and myHero.pos:DistanceTo(minion.pos) > 200 and IsValid(minion) and Ready(_Q) then
-                local QDmg = getdmg("Q", minion, myHero)
-				if QDmg >= minion.health then
-					Control.CastSpell(HK_Q, minion)
-				end	
             end
-			
-            if Menu.Last.UseW:Value() and mana_ok and myHero.pos:DistanceTo(minion.pos) < 200 and IsValid(minion) and Ready(_W) then
-				local Dmg = (getdmg("W", minion, myHero, 1) + getdmg("W", minion, myHero, 2) + getdmg("AA", minion, myHero))
-				if Dmg >= minion.health then	
-					Control.CastSpell(HK_W)
-				end	
-            end			
+
+			if HasBuff(myHero, "qiyanawenchantedbuff") and myHero.pos:DistanceTo(minion.pos) < 710 then
+				Control.CastSpell(HK_Q, minion.pos)
+			end				
         end
     end
 end
@@ -254,45 +248,44 @@ local target = GetTarget(800)
 if target == nil then return end
 	if IsValid(target) then
        
-	local stacks = GotBuff(myHero, "RiftWalk")
-	local QDmg = getdmg("Q", target, myHero)
-	local RDmg = getdmg("R", target, myHero, 1)
-	local RBonusDmg = getdmg("R", target, myHero, 2)
-	local WDmg = (getdmg("W", target, myHero, 1) + getdmg("W", target, myHero, 2) + getdmg("AA", target, myHero))
-	local FullRDmg = ((stacks * RBonusDmg) + RDmg)
-	local RWDmg = (WDmg + FullRDmg)
+	local QDmg = getdmg("Q", target, myHero, 1)
+	local Q2Dmg = getdmg("Q", target, myHero, 2)
+	local EDmg = getdmg("E", target, myHero)
+	local WPassiveDmg = getdmg("W", target, myHero)
+	local EWDmg = (EDmg + WPassiveDmg)	
 	local HP = (target.health + (target.hpRegen * 2))	
-		if Menu.ks.UseQ:Value() and myHero.pos:DistanceTo(target.pos) < 650 and Ready(_Q) then
+		
+		if Menu.ks.UseQ:Value() and myHero.pos:DistanceTo(target.pos) < 650 and Ready(_Q) and not HasBuff(myHero, "qiyanawenchantedbuff") then
 			if QDmg-20 >= HP then
-				Control.CastSpell(HK_Q, target)
+				Control.CastSpell(HK_Q, target.pos)
 			end	
         end
 		
-        if Menu.ks.UseRW:Value() and myHero.pos:DistanceTo(target.pos) < 650 then
-	
-			if stacks >= 1 then
-
-				if Ready(_R) and Ready(_W) and RWDmg >= HP then
-					Control.CastSpell(HK_R, target.pos)
-				end	
-				if Ready(_W) and myHero.pos:DistanceTo(target.pos) < 200 and WDmg >= target.health then
-					Control.CastSpell(HK_W)
-				end
-			else
-				if Ready(_R) and Ready(_W) and (RDmg + WDmg) >= HP then
-					Control.CastSpell(HK_R, target.pos)
-				end	
-				if Ready(_W) and myHero.pos:DistanceTo(target.pos) < 200 and WDmg >= target.health then
-					Control.CastSpell(HK_W)
-				end				
-			end
-		end
+		if Menu.ks.UseQ:Value() and myHero.pos:DistanceTo(target.pos) < 710 and HasBuff(myHero, "qiyanawenchantedbuff") and not myHero:GetSpellData(_Q).name == "QiyanaQ_Rock" then
+			if QDmg-20 >= HP then
+				Control.CastSpell(HK_Q, target.pos)
+			end	
+        end		
 		
-		if Menu.ks.UseRQ:Value() and myHero.pos:DistanceTo(target.pos) > 650 and myHero.pos:DistanceTo(target.pos) < 1100 then
-			if Ready(_R) and Ready(_Q) and QDmg-20 >= HP then
-				Control.CastSpell(HK_R, target.pos)
+		if Menu.ks.UseQ2:Value() and myHero.pos:DistanceTo(target.pos) < 710 and myHero:GetSpellData(_Q).name == "QiyanaQ_Rock" then
+			if Q2Dmg-20 >= HP then
+				Control.CastSpell(HK_Q, target.pos)
 			end			
         end
+		
+		if HasBuff(myHero, "qiyanawenchantedbuffhaste") then
+			if Menu.ks.UseE:Value() and myHero.pos:DistanceTo(target.pos) < 650 and Ready(_E) then
+				if EWDmg-20 >= HP then
+					Control.CastSpell(HK_E, target)
+				end	
+			end
+		else
+			if Menu.ks.UseE:Value() and myHero.pos:DistanceTo(target.pos) < 650 and Ready(_E) then
+				if EDmg-20 >= HP then
+					Control.CastSpell(HK_E, target)
+				end	
+			end		
+		end
 	end	
 end
-]]
+
