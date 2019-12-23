4a4d292ae5798128fbdc0444da5bbaa4127a15de
Pussykate <35004487+Pussykate@users.noreply.github.com>
2019-12-23 18:10:50 +0100

    Update currentVersion.lua

--- AIOChampions/currentVersion.lua
+++ AIOChampions/currentVersion.lua
@@ -75,7 +75,7 @@ Data = {
         }, 
         Malzahar = {
             Changelog = "Malzahar Changelog Here",
-            Version = 0.01,
+            Version = 0.02,
         },        
     },
 }
