bf2c58c8f4d3818dc2317979f27533d4cf866dea
Pussykate <35004487+Pussykate@users.noreply.github.com>
2019-12-23 13:36:24 +0100

    Update Tristana.lua

--- AIOChampions/Champions/Tristana.lua
+++ AIOChampions/Champions/Tristana.lua
@@ -35,13 +35,24 @@ end
 function LoadScript()
 
 	Menu = MenuElement({type = MENU, id = myHero.networkID, name = myHero.charName})
-	Menu:MenuElement({name = " ", drop = {"Version 0.02"}})	
+	Menu:MenuElement({name = " ", drop = {"Version 0.03"}})	
 	
 	Menu:MenuElement({type = MENU, id = "Combo", name = "Combo"})
 	Menu.Combo:MenuElement({id = "UseQ", name = "AutoQ when Explosive Charge", value = true})
 	Menu.Combo:MenuElement({id = "UseE", name = "E", value = true})
 	Menu.Combo:MenuElement({id = "UseR", name = "(R)Finisher", tooltip = "is(R)Dmg+(E)Dmg+(E)StackDmg > TargetHP than Ult", value = true})
 	Menu.Combo:MenuElement({id = "comboActive", name = "Combo key", key = string.byte(" ")})
+	
+	Menu:MenuElement({type = MENU, id = "Harass", name = "Harass"})
+	Menu.Harass:MenuElement({id = "UseQ", name = "AutoQ when Explosive Charge", value = true})
+	Menu.Harass:MenuElement({id = "UseE", name = "E", value = true})
+	Menu.Harass:MenuElement({id = "harassActive", name = "Harass key", key = string.byte("C")})	
+	
+	Menu:MenuElement({type = MENU, id = "Clear", name = "LaneClear"})	
+	Menu.Clear:MenuElement({id = "UseQ", name = "[Q]", value = true})		
+	Menu.Clear:MenuElement({id = "UseE", name = "[E] Cannon Minions", value = true}) 		
+	Menu.Clear:MenuElement({id = "Mana", name = "Min Mana to LaneClear", value = 40, min = 0, max = 100, identifier = "%"})	
+	Menu.Clear:MenuElement({id = "clearActive", name = "Clear key", key = string.byte("V")})	
 	
 	Menu:MenuElement({type = MENU, id = "gap", name = "Gapclose"})
 	Menu.gap:MenuElement({name = " ", drop = {"Use AutoW if KillableTarget > UltiRange"}})		
@@ -52,17 +63,12 @@ function LoadScript()
 	
 	Menu:MenuElement({type = MENU, id = "Blitz", name = "Escape"})
 	Menu.Blitz:MenuElement({id = "UseW", name = "AutoW ( Blitzcrank Grab )", value = true})
-	
-	Menu:MenuElement({type = MENU, id = "Harass", name = "Harass"})
-	Menu.Harass:MenuElement({id = "UseQ", name = "AutoQ when Explosive Charge", value = true})
-	Menu.Harass:MenuElement({id = "UseE", name = "E", value = true})
-	Menu.Harass:MenuElement({id = "harassActive", name = "Harass key", key = string.byte("C")})
 
 	Menu:MenuElement({type = MENU, id = "Drawings", name = "Drawings"})
 	
 	--W
 	Menu.Drawings:MenuElement({type = MENU, id = "W", name = "Draw W range"})
-    Menu.Drawings.W:MenuElement({id = "Enabled", name = "Enabled", value = true})       
+    Menu.Drawings.W:MenuElement({id = "Enabled", name = "Enabled", value = false})       
     Menu.Drawings.W:MenuElement({id = "Width", name = "Width", value = 1, min = 1, max = 5, step = 1})
     Menu.Drawings.W:MenuElement({id = "Color", name = "Color", color = Draw.Color(200, 255, 255, 255)})
 	--E
@@ -72,7 +78,7 @@ function LoadScript()
     Menu.Drawings.E:MenuElement({id = "Color", name = "Color", color = Draw.Color(200, 255, 255, 255)})	
 	--R
 	Menu.Drawings:MenuElement({type = MENU, id = "R", name = "Draw R range"})
-    Menu.Drawings.R:MenuElement({id = "Enabled", name = "Enabled", value = true})
+    Menu.Drawings.R:MenuElement({id = "Enabled", name = "Enabled", value = false})
     Menu.Drawings.R:MenuElement({id = "Width", name = "Width", value = 1, min = 1, max = 5, step = 1})
     Menu.Drawings.R:MenuElement({id = "Color", name = "Color", color = Draw.Color(200, 255, 255, 255)})
 	
@@ -119,9 +125,9 @@ local Mode = GetMode()
 			HarassE()
 		end
 	elseif Mode == "Clear" then
-	
-	elseif Mode == "Flee" then
-		
+		if Menu.Clear.clearActive:Value() then		
+			Clear()
+		end
 	end
 
 	if Menu.Blitz.UseW:Value() then
@@ -173,7 +179,7 @@ end
 function CheckSpell(range)
 	for i = 1,Game.HeroCount() do
 		local hero = Game.Hero(i)
-        if hero.team ~= myHero.team then
+        if hero.team == TEAM_ENEMY and myHero.pos:DistanceTo(hero.pos) <= range and IsValid(hero) then
 			if hero.activeSpell.name == "RocketGrab" then 
 				casterPos = hero.pos
 				grabTime = hero.activeSpell.startTime * 100
@@ -189,19 +195,15 @@ local timer = {state = false, tick = GetTickCount(), mouse = mousePos, done = fa
 function AntiBlitz()	
 	if GetTickCount() - timer.tick > 300 and GetTickCount() - timer.tick < 700 then 
 		timer.state = false
-		_G.SDK.Orbwalker:SetMovement(true)
-		_G.SDK.Orbwalker:SetAttack(true)
 	end
 
 	local ctc = Game.Timer() * 100
 	
-	local target = GetTarget(900)
-	if Menu.Blitz.UseW:Value() and CheckSpell(900) and grabTime ~= nil and Ready(_W) then 
+	local target = GetTarget(1200)
+	if Menu.Blitz.UseW:Value() and CheckSpell(1200) and grabTime ~= nil and Ready(_W) then 
 		if myHero.pos:DistanceTo(target.pos) > 350 then
 			if ctc - grabTime >= 28 then
 				local jump = myHero.pos:Shortened(target.pos, 700)
-				_G.SDK.Orbwalker:SetMovement(false)
-				_G.SDK.Orbwalker:SetAttack(false)
 				Control.SetCursorPos(jump)
 				Control.KeyDown(HK_W)
 				Control.KeyUp(HK_W)
@@ -209,8 +211,6 @@ function AntiBlitz()
 		else
 			if ctc - grabTime >= 12 then
 				local jump = myHero.pos:Shortened(target.pos, 700)
-				_G.SDK.Orbwalker:SetMovement(false)
-				_G.SDK.Orbwalker:SetAttack(false)
 				Control.SetCursorPos(jump)
 				Control.KeyDown(HK_W)
 				Control.KeyUp(HK_W)
@@ -311,4 +311,21 @@ if target == nil then return end
 		Control.CastSpell(HK_E, target)
 		   
 	end
+end
+
+function Clear()
+    for i = 1, Game.MinionCount() do
+    local minion = Game.Minion(i)
+        if myHero.pos:DistanceTo(minion.pos) < E.Range and minion.team == TEAM_ENEMY and IsValid(minion) then
+            local mana_ok = myHero.mana/myHero.maxMana >= Menu.Clear.Mana:Value() / 100
+            
+            if Menu.Clear.UseE:Value() and mana_ok and Ready(_E) and minion.charName == "SRU_ChaosMinionSiege" then
+				Control.CastSpell(HK_E, minion)
+            end		
+
+			if Menu.Clear.UseQ:Value() and mana_ok and Ready(_Q) then
+				Control.CastSpell(HK_Q)
+			end				
+        end
+    end
 end
