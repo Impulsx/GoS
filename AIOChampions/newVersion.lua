70e50bb2cc2d5ca659c47a601f79eb4c1e13f831
Pussykate <35004487+Pussykate@users.noreply.github.com>
2019-12-23 22:19:20 +0100

    Update newVersion.lua

--- AIOChampions/newVersion.lua
+++ AIOChampions/newVersion.lua
@@ -23,7 +23,7 @@ Data = {
         },
         Soraka = {
             Changelog = "Soraka Changelog Here",
-            Version = 0.03,
+            Version = 0.04,
         },
         Zyra = {
             Changelog = "Zyra Changelog Here",
