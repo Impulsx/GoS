c97c934ef5512f1c8688cfbce5eee0200fb8291e
Pussykate <35004487+Pussykate@users.noreply.github.com>
2019-12-30 17:13:11 +0100

    Update Morgana.lua

--- AIOChampions/Champions/Morgana.lua
+++ AIOChampions/Champions/Morgana.lua
@@ -227,8 +227,8 @@ function LoadScript()
 	OnProcessSpell() 
 	DetectedMissiles = {}; DetectedSpells = {}; Target = nil; Timer = 0	 
 	
-	Menu = MenuElement({type = MENU, id = myHero.networkID, name = myHero.charName})
-	Menu:MenuElement({name = " ", drop = {"Version 0.03"}})
+	Menu = MenuElement({type = MENU, id = "PussyAIO".. myHero.charName, name = myHero.charName})
+	Menu:MenuElement({name = " ", drop = {"Version 0.04"}})
 	
 	--AutoE
 	Menu:MenuElement({id = "AutoE", name = "AutoE if Ground Controled", type = MENU})
