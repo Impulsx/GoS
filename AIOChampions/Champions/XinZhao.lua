4beff25abab2fd004b89a4979ea76694d6fe3d56
Pussykate <35004487+Pussykate@users.noreply.github.com>
2019-12-30 17:21:27 +0100

    Update XinZhao.lua

--- AIOChampions/Champions/XinZhao.lua
+++ AIOChampions/Champions/XinZhao.lua
@@ -23,8 +23,8 @@ end
 
 function LoadScript()
 	
-	Menu = MenuElement({type = MENU, id = myHero.networkID, name = myHero.charName})
-	Menu:MenuElement({name = " ", drop = {"Version 0.02"}})	
+	Menu = MenuElement({type = MENU, id = "PussyAIO".. myHero.charName, name = myHero.charName})
+	Menu:MenuElement({name = " ", drop = {"Version 0.03"}})	
 	Menu:MenuElement({type = MENU, id = "Mode", name = myHero.charName})
 	
 	--Main Menu-- PussyXinZhao -- AutoW
