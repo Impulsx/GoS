34a64032ade2800e651d95e54ffbfa0da67b6faf
Pussykate <35004487+Pussykate@users.noreply.github.com>
2019-12-30 17:10:10 +0100

    Update Jhin.lua

--- AIOChampions/Champions/Jhin.lua
+++ AIOChampions/Champions/Jhin.lua
@@ -108,8 +108,8 @@ function UseDarkHarvest()
 end
 
 function LoadScript()
-	Menu = MenuElement({type = MENU, id = myHero.networkID, name = myHero.charName})
-	Menu:MenuElement({name = " ", drop = {"Version 0.03"}})	
+	Menu = MenuElement({type = MENU, id = "PussyAIO".. myHero.charName, name = myHero.charName})
+	Menu:MenuElement({name = " ", drop = {"Version 0.04"}})	
 	
 	--AutoW 
 	Menu:MenuElement({type = MENU, id = "AutoW", name = "Stack Dark Harvest"})
