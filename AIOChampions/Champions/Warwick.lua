d9961f85cd36ae532400e2bdcb81baec1653f29a
Pussykate <35004487+Pussykate@users.noreply.github.com>
2019-12-30 17:20:59 +0100

    Update Warwick.lua

--- AIOChampions/Champions/Warwick.lua
+++ AIOChampions/Champions/Warwick.lua
@@ -41,8 +41,8 @@ end
 
 function LoadScript()
 	HPred()
-	Menu = MenuElement({type = MENU, id = myHero.networkID, name = myHero.charName})
-	Menu:MenuElement({name = " ", drop = {"Version 0.03"}})	
+	Menu = MenuElement({type = MENU, id = "PussyAIO".. myHero.charName, name = myHero.charName})
+	Menu:MenuElement({name = " ", drop = {"Version 0.04"}})	
 	
 	--Combo--
 	Menu:MenuElement({id = "ComboMode", name = "Combo", type = MENU})
