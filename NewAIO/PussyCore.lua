9ba8e88e4f09a1895c93665d0f2128d3c735bf3b
Pussykate <35004487+Pussykate@users.noreply.github.com>
2020-01-11 20:47:48 +0100

    Add files via upload

---
+++ NewAIO/PussyCore.lua
@@ -0,0 +1,160 @@
+local Core = {}
+
+
+local Orb
+local Core:TEAM_ALLY = myHero.team
+local Core:TEAM_ENEMY = 300 - myHero.team
+local Core:TEAM_JUNGLE = 300
+
+
+function Core:HasBuff(unit, buffname)
+	for i = 0, unit.buffCount do
+		local buff = unit:GetBuff(i)
+		if buff.name == buffname and buff.count > 0 then 
+			return true
+		end
+	end
+	return false
+end			
+
+function Core:CheckWall(from, to, distance)
+    local pos = to + (to - from):Normalized() * distance
+    return MapPosition:intersectsWall(to, pos) --to: enemy
+end
+
+function Core:IsUnderTurret(unit)
+    for i = 1, Game.TurretCount() do
+        local turret = Game.Turret(i)
+        local range = (turret.boundingRadius + 750 + unit.boundingRadius / 2)
+        if turret.isEnemy and not turret.dead then
+            if turret.pos:DistanceTo(unit.pos) < range then
+                return true
+            end
+        end
+    end
+    return false
+end
+
+function Core:AllyMinionUnderTower()
+	for i = 1, Game.MinionCount() do
+    local minion = Game.Minion(i)
+		if minion.team == Core:TEAM_ALLY and IsValid(minion) and IsUnderTurret(minion) and myHero.pos:DistanceTo(minion.pos) <= 750 then
+			return true
+		end
+	end
+	return false
+end
+
+function Core:SetAttack(bool)
+	if _G.EOWLoaded then
+		EOW:SetAttacks(bool)
+	elseif _G.SDK then                                                        
+		_G.SDK.Orbwalker:SetAttack(bool)
+	elseif _G.gsoSDK then
+		_G.gsoSDK.Orbwalker:SetAttack(bool)	
+	else
+		GOS.BlockAttack = not bool
+	end
+end
+
+local function IsRecalling(unit)
+	for i = 1, 63 do
+	local buff = unit:GetBuff(i) 
+		if buff.count > 0 and buff.name == "recall" and Game.Timer() < buff.expireTime then
+			return true
+		end
+	end 
+	return false
+end
+
+function Core:MyHeroNotReady()
+    return myHero.dead or Game.IsChatOpen() or (_G.JustEvade and _G.JustEvade:Evading()) or (_G.ExtLibEvade and _G.ExtLibEvade.Evading) or IsRecalling(myHero)
+end
+
+function Core:GetMode()
+    if Orb == 1 then
+        if combo == 1 then
+            return 'Combo'
+        elseif harass == 2 then
+            return 'Harass'
+        elseif lastHit == 3 then
+            return 'Lasthit'
+        elseif laneClear == 4 then
+            return 'Clear'
+        end
+    elseif Orb == 2 then
+		if _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_COMBO] then
+			return "Combo"
+		elseif _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_HARASS] then
+			return "Harass"	
+		elseif _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_LANECLEAR] or _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_JUNGLECLEAR] then
+			return "Clear"
+		elseif _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_LASTHIT] then
+			return "LastHit"
+		elseif _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_FLEE] then
+			return "Flee"
+		end
+    elseif Orb == 3 then
+        return GOS:GetMode()
+    elseif Orb == 4 then
+        return _G.gsoSDK.Orbwalker:GetMode()
+    end
+end
+
+function Core:Ready(slot)
+    return myHero:GetSpellData(spell).currentCd == 0 and myHero:GetSpellData(spell).level > 0 and myHero:GetSpellData(spell).mana <= myHero.mana
+end
+
+function Core:IsValid(unit)
+    if (unit and unit.valid and unit.isTargetable and unit.alive and unit.visible and unit.networkID and unit.pathing and unit.health > 0) then
+        return true;
+    end
+    return false;
+end
+
+function Core:GetTarget(range)
+	if Orb == 1 then
+		if myHero.ap > myHero.totalDamage then
+			return EOW:GetTarget(range, EOW.ap_dec, myHero.pos)
+		else
+			return EOW:GetTarget(range, EOW.ad_dec, myHero.pos)
+		end
+	elseif Orb == 2 and SDK.TargetSelector then
+		if myHero.ap > myHero.totalDamage then
+			return SDK.TargetSelector:GetTarget(range, _G.SDK.DAMAGE_TYPE_MAGICAL)
+		else
+			return SDK.TargetSelector:GetTarget(range, _G.SDK.DAMAGE_TYPE_PHYSICAL)
+		end
+	elseif _G.GOS then
+		if myHero.ap > myHero.totalDamage then
+			return GOS:GetTarget(range, "AP")
+		else
+			return GOS:GetTarget(range, "AD")
+        end
+    elseif _G.gsoSDK then
+		return _G.gsoSDK.TS:GetTarget()
+	end
+end
+
+function Core:Rotate(startPos, endPos, height, theta)
+    local dx, dy = endPos.x - startPos.x, endPos.z - startPos.z
+    local px, py = dx * math.cos(theta) - dy * math.sin(theta), dx * math.sin(theta) + dy * math.cos(theta)
+    return Vector(px + startPos.x, height, py + startPos.z)
+end
+
+function Core:GetDistanceSqr(p1, p2)
+	if not p1 then return math.huge end
+	p2 = p2 or myHero
+	local dx = p1.x - p2.x
+	local dz = (p1.z or p1.y) - (p2.z or p2.y)
+	return dx*dx + dz*dz
+end
+
+function Core:GetDistance(p1, p2)
+	p2 = p2 or myHero
+	return math.sqrt(GetDistanceSqr(p1, p2))
+end
+
+function Core:GetDistance2D(p1,p2)
+	return math.sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y))
+end
