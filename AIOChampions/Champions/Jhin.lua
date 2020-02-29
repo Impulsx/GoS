d4bd9a407cc0cb7f84d0099b6661ef077f7fdba2
Pussykate <35004487+Pussykate@users.noreply.github.com>
2020-02-29 14:55:32 +0100

    Update Jhin.lua

--- AIOChampions/Champions/Jhin.lua
+++ AIOChampions/Champions/Jhin.lua
@@ -1,9 +1,9 @@
-function CastSpell(spell,pos,range,delay)
-    local range = range or math.huge
+local function CastSpell(spell,pos,range,delay)
+    local range = range or MathHuge
     local delay = delay or 250
     local ticker = GetTickCount()
 
-    if castSpell.state == 0 and GetDistance(myHero.pos,pos) < range and ticker - castSpell.casting > delay + Game.Latency() and pos:ToScreen().onScreen then
+    if castSpell.state == 0 and GetDistance(myHero.pos,pos) < range and ticker - castSpell.casting > delay + Latency() and pos:ToScreen().onScreen then
         castSpell.state = 1
         castSpell.mouse = mousePos
         castSpell.tick = ticker
@@ -19,32 +19,32 @@ function CastSpell(spell,pos,range,delay)
                     Control.SetCursorPos(castSpell.mouse)
                     castSpell.state = 0
                 end
-            end,Game.Latency()/1000)
+            end,Latency()/1000)
         end
-        if ticker - castSpell.casting > Game.Latency() then
+        if ticker - castSpell.casting > Latency() then
             Control.SetCursorPos(castSpell.mouse)
             castSpell.state = 0
         end
     end
 end
 
-function CastSpellStart(spell, pos)
+local function CastSpellStart(spell, pos)
 	_G.SDK.Orbwalker:SetMovement(false)
-	Control.CastSpell(spell, pos)
+	ControlCastSpell(spell, pos)
 	_G.SDK.Orbwalker:SetMovement(true)
 end	
 
-function CastSpellMM(spell,pos,range,delay)
-	local range = range or math.huge
+local function CastSpellMM(spell,pos,range,delay)
+	local range = range or MathHuge
 	local delay = delay or 250
 	local ticker = GetTickCount()
-	if castSpell.state == 0 and GetDistance(myHero.pos,pos) < range and ticker - castSpell.casting > delay + Game.Latency() then
+	if castSpell.state == 0 and GetDistance(myHero.pos,pos) < range and ticker - castSpell.casting > delay + Latency() then
 		castSpell.state = 1
 		castSpell.mouse = mousePos
 		castSpell.tick = ticker
 	end
 	if castSpell.state == 1 then
-		if ticker - castSpell.tick < Game.Latency() then
+		if ticker - castSpell.tick < Latency() then
 			local castPosMM = pos:ToMM()
 			Control.SetCursorPos(castPosMM.x,castPosMM.y)
 			Control.KeyDown(spell)
@@ -55,16 +55,16 @@ function CastSpellMM(spell,pos,range,delay)
 					Control.SetCursorPos(castSpell.mouse)
 					castSpell.state = 0
 				end
-			end,Game.Latency()/1000)
+			end,Latency()/1000)
 		end
-		if ticker - castSpell.casting > Game.Latency() then
+		if ticker - castSpell.casting > Latency() then
 			Control.SetCursorPos(castSpell.mouse)
 			castSpell.state = 0
 		end
 	end
 end
 
-function HasBuff(unit, buffname)
+local function HasBuff(unit, buffname)
 	for i = 0, unit.buffCount do
 		local buff = unit:GetBuff(i)
 		if buff.name == buffname and buff.count > 0 then 
@@ -74,11 +74,11 @@ function HasBuff(unit, buffname)
 	return false
 end
  
-function GetMinionCount(range, pos)
+local function GetMinionCount(range, pos)
     local pos = pos.pos
 	local count = 0
-	for i = 1,Game.MinionCount() do
-	local hero = Game.Minion(i)
+	for i = 1,GameMinionCount() do
+	local hero = GameMinion(i)
 	local Range = range * range
 		if hero.team ~= TEAM_ALLY and hero.dead == false and GetDistanceSqr(pos, hero.pos) < Range then
 		count = count + 1
@@ -87,7 +87,7 @@ function GetMinionCount(range, pos)
 	return count
 end
 
-function GetAngle(v1, v2)
+local function GetAngle(v1, v2)
 	local vec1 = v1:Len()
 	local vec2 = v2:Len()
 	local Angle = math.abs(math.deg(math.acos((v1*v2)/(vec1*vec2))))
@@ -97,7 +97,7 @@ function GetAngle(v1, v2)
 	return false
 end
 
-function UseDarkHarvest()
+local function UseDarkHarvest()
 	for i = 0, myHero.buffCount do
 		local buff = myHero:GetBuff(i)
 		if buff.name:lower():find("darkharvest") and buff.count > 0 then 
@@ -109,7 +109,7 @@ end
 
 function LoadScript()
 	Menu = MenuElement({type = MENU, id = "PussyAIO".. myHero.charName, name = myHero.charName})
-	Menu:MenuElement({name = " ", drop = {"Version 0.04"}})	
+	Menu:MenuElement({name = " ", drop = {"Version 0.05"}})	
 	
 	--AutoW 
 	Menu:MenuElement({type = MENU, id = "AutoW", name = "Stack Dark Harvest"})
@@ -151,6 +151,7 @@ function LoadScript()
 	
 	--Prediction
 	Menu:MenuElement({type = MENU, id = "Pred", name = "Prediction"})
+	Menu.Pred:MenuElement({id = "Change", name = "Change Prediction Typ", value = 1, drop = {"Gamsteron Prediction", "Premium Prediction"}})	
 	Menu.Pred:MenuElement({id = "PredW", name = "Hitchance[W]", value = 1, drop = {"Normal", "High", "Immobile"}})	
 	Menu.Pred:MenuElement({id = "PredR", name = "Hitchance[R]", value = 1, drop = {"Normal", "High", "Immobile"}})
  
@@ -165,11 +166,15 @@ function LoadScript()
 	{
 	Type = _G.SPELLTYPE_LINE, Delay = 0.66, Radius = 40, Range = 3000, Speed = 5000, Collision = false
 	}
+	
+	WspellData = {speed = 5000, range = 3000, delay = 0.66, radius = 40, collision = {}, type = "linear"}		
 
 	RData =
 	{
 	Type = _G.SPELLTYPE_LINE, Delay = 0.25, Radius = 80, Range = 3500, Speed = 5000, Collision = false
 	}
+	
+	RspellData = {speed = 5000, range = 3500, delay = 0.25, radius = 80, collision = {}, type = "linear"}		
   	
 	if _G.EOWLoaded then
 		Orb = 1
@@ -186,21 +191,17 @@ function LoadScript()
 	if myHero.dead then return end
 	
 		if Menu.Drawing.DrawR:Value() and Ready(_R) then
-		Draw.Circle(myHero, 3500, 1, Draw.Color(255, 225, 255, 10))
+		DrawCircle(myHero, 3500, 1, DrawColor(255, 225, 255, 10))
 		end                                                 
 		if Menu.Drawing.DrawQ:Value() and Ready(_Q) then
-		Draw.Circle(myHero, 600, 1, Draw.Color(225, 225, 0, 10))
+		DrawCircle(myHero, 600, 1, DrawColor(225, 225, 0, 10))
 		end
 		if Menu.Drawing.DrawE:Value() and Ready(_E) then
-		Draw.Circle(myHero, 750, 1, Draw.Color(225, 225, 125, 10))
+		DrawCircle(myHero, 750, 1, DrawColor(225, 225, 125, 10))
 		end
 		if Menu.Drawing.DrawW:Value() and Ready(_W) then
-		Draw.Circle(myHero, 3000, 1, Draw.Color(225, 225, 125, 10))
+		DrawCircle(myHero, 3000, 1, DrawColor(225, 225, 125, 10))
 		end
-		local textPos = myHero.pos:To2D()	
-		if not FileExist(COMMON_PATH .. "GamsteronPrediction.lua") then
-			Draw.Text("GsoPred. installed Press 2x F6", 50, textPos.x + 100, textPos.y - 250, Draw.Color(255, 255, 0, 0))
-		end	
 		
 		local target = GetTarget(20000)
 		if target == nil then return end	
@@ -208,8 +209,8 @@ function LoadScript()
 		local Dmg = getdmg("R", target, myHero, 1)
 		local hp = (target.health)	
 			if Ready(_R) and (5*Dmg) > hp then
-				Draw.Text("Ult Kill", 24, target.pos2D.x, target.pos2D.y,Draw.Color(0xFF00FF00))
-				Draw.Text("Ult Kill", 13, target.posMM.x - 15, target.posMM.y - 15,Draw.Color(0xFF00FF00))
+				DrawText("Ult Kill", 24, target.pos2D.x, target.pos2D.y,DrawColor(0xFF00FF00))
+				DrawText("Ult Kill", 13, target.posMM.x - 15, target.posMM.y - 15,DrawColor(0xFF00FF00))
 			end	
 		end
 	end)		
@@ -255,7 +256,7 @@ if target == nil then return end
 			CastSpell(HK_R, target.pos, 3500)
         else
 			local castPos = myHero.pos:Extended(target.pos, 1000)    
-			Control.CastSpell(HK_R, castPos)
+			ControlCastSpell(HK_R, castPos)
                             
         end
 		
@@ -267,7 +268,7 @@ if target == nil then return end
 	end	
 end
 
-function DarkHarvestReady()
+local function DarkHarvestReady()
 	for i = 0, myHero.buffCount do
 		local buff = myHero:GetBuff(i)
 		if buff.name:lower():find("darkharvestcooldown") and buff.count > 0 then 
@@ -305,14 +306,14 @@ if target == nil or myHero:GetSpellData(_R).name == "JhinRShot" then return end
 			if Menu.Combo.UseQ:Value() ~= 1 then
 				if IsCastingAA("JhinPassiveAttack") then
 					DelayAction(function()
-					Control.CastSpell(HK_Q, target)
-					end,Game.Latency()/100)
+					ControlCastSpell(HK_Q, target)
+					end,Latency()/100)
 				end
 			else 
 				if IsCastingAA("JhinBasicAttack3") then
 					DelayAction(function()
-						Control.CastSpell(HK_Q, target)
-					end,Game.Latency()/100)
+						ControlCastSpell(HK_Q, target)
+					end,Latency()/100)
 				end
 			end	
 		end
@@ -334,13 +335,13 @@ function Harass()
 local target = GetTarget(1000)
 if target == nil or myHero:GetSpellData(_R).name == "JhinRShot" then return end
 	if IsValid(target) and myHero.mana/myHero.maxMana >= Menu.Harass.Mana:Value() / 100 then
-		for i = 1, Game.MinionCount() do
-		local minion = Game.Minion(i)
+		for i = 1, GameMinionCount() do
+		local minion = GameMinion(i)
 			if myHero.pos:DistanceTo(minion.pos) <= 550 and minion.team == TEAM_ENEMY and IsValid(minion) and Ready(_Q) then
 			local QDmg = getdmg("Q", minion, myHero)
 			local count = GetMinionCount(400, minion)
 				if QDmg >= minion.health and target.pos:DistanceTo(minion.pos) <= 400 and count <= 3 then
-					Control.CastSpell(HK_Q, minion)
+					ControlCastSpell(HK_Q, minion)
 				end	
 			end
 		end	
@@ -360,8 +361,8 @@ end
 
 function Clear()
     if myHero:GetSpellData(_R).name == "JhinRShot" then return end
-	for i = 1, Game.MinionCount() do
-    local minion = Game.Minion(i)
+	for i = 1, GameMinionCount() do
+    local minion = GameMinion(i)
 	
 		if myHero.pos:DistanceTo(minion.pos) <= 800 and minion.team == TEAM_ENEMY and IsValid(minion) then
             
@@ -370,7 +371,7 @@ function Clear()
                 local QDmg = getdmg("Q", minion, myHero)
 				local count = GetMinionCount(400, minion)
 				if QDmg >= minion.health and count >= Menu.Clear.UseQM:Value() then
-					Control.CastSpell(HK_Q, minion)
+					ControlCastSpell(HK_Q, minion)
 				end	
             end
             local count = GetMinionCount(260, minion)          
@@ -384,8 +385,8 @@ end
 
 function LastHitW()
 	if myHero:GetSpellData(_R).name == "JhinRShot" then return end
-	for i = 1, Game.MinionCount() do
-    local minion = Game.Minion(i)
+	for i = 1, GameMinionCount() do
+    local minion = GameMinion(i)
 		if myHero.pos:DistanceTo(minion.pos) <= 3000 and minion.team == TEAM_ENEMY and minion.charName == "SRU_ChaosMinionSiege" and IsValid(minion) then
 			local WDmg = getdmg("W", minion, myHero, 2)
 			if myHero.pos:DistanceTo(minion.pos) > 550 and WDmg >= minion.health then
@@ -412,24 +413,45 @@ function KillSteal()
 end	
 
 function CastW(target)
-	local pred = GetGamsteronPrediction(target, WData, myHero)
-	if pred.Hitchance >= Menu.Pred.PredW:Value() + 1 then
-		CastSpellStart(HK_W, pred.CastPosition)
+	if Menu.Pred.Change:Value() == 1 then
+		local pred = GetGamsteronPrediction(target, WData, myHero)
+		if pred.Hitchance >= Menu.Pred.PredW:Value()+1 then
+			CastSpellStart(HK_W, pred.CastPosition)
+		end
+	else
+		local pred = _G.PremiumPrediction:GetPrediction(myHero, target, WspellData)
+		if pred.CastPos and ConvertToHitChance(Menu.Pred.PredW:Value(), pred.HitChance) then
+			CastSpellStart(HK_W, pred.CastPos)
+		end	
 	end
 end	
 
 function CastR(target)	
-	local pred = GetGamsteronPrediction(target, RData, myHero)
 	local angle = GetAngle(myHero.pos, target.pos)
-	if myHero.pos:DistanceTo(target.pos) <= 3500 and IsValid(target) and pred.Hitchance >= Menu.Pred.PredR:Value() + 1 and angle then
-		_G.SDK.Orbwalker:SetMovement(false)
-		if target.pos:To2D().onScreen then
-			CastSpell(HK_R, pred.CastPosition)
-        else
-			CastSpellMM(HK_R, pred.CastPosition)
-                            
-        end
-		_G.SDK.Orbwalker:SetMovement(true)
+	if myHero.pos:DistanceTo(target.pos) <= 3500 and IsValid(target) and angle then
+		if Menu.Pred.Change:Value() == 1 then
+			local pred = GetGamsteronPrediction(target, RData, myHero)
+			if pred.Hitchance >= Menu.Pred.PredR:Value()+1 then
+				_G.SDK.Orbwalker:SetMovement(false)
+				if target.pos:To2D().onScreen then
+					CastSpell(HK_R, pred.CastPosition)
+				else
+					CastSpellMM(HK_R, pred.CastPosition)                            
+				end
+				_G.SDK.Orbwalker:SetMovement(true)
+			end
+		else
+			local pred = _G.PremiumPrediction:GetPrediction(myHero, target, RspellData)
+			if pred.CastPos and ConvertToHitChance(Menu.Pred.PredR:Value(), pred.HitChance) then
+				_G.SDK.Orbwalker:SetMovement(false)
+				if target.pos:To2D().onScreen then
+					CastSpell(HK_R, pred.CastPos)
+				else
+					CastSpellMM(HK_R, pred.CastPos)                            
+				end
+				_G.SDK.Orbwalker:SetMovement(true)
+			end	
+		end	
 	end
 end	
 
