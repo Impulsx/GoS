6177a6ad98e18f348b4b3bab3c5dced5de9190b8
Pussykate <35004487+Pussykate@users.noreply.github.com>
2020-02-29 18:22:11 +0100

    Update Sona.lua

--- AIOChampions/Champions/Sona.lua
+++ AIOChampions/Champions/Sona.lua
@@ -1,19 +1,12 @@
-function GetAllyHeroes() 
-	AllyHeroes = {}
-	for i = 1, Game.HeroCount() do
-		local Hero = Game.Hero(i)
-		if Hero.isAlly and not Hero.isMe then
-			table.insert(AllyHeroes, Hero)
-		end
-	end
-	return AllyHeroes
+local function GetAllyHeroes() 
+	return Allies
 end
 
-function CountEnemiesNear(pos, range)
+local function CountEnemiesNear(pos, range)
     local pos = pos.pos
 	local count = 0
-	for i = 1, Game.HeroCount() do 
-	local hero = Game.Hero(i)
+	for i = 1, GameHeroCount() do 
+	local hero = GameHero(i)
 	local Range = range * range
 		if hero.team ~= TEAM_ALLY and GetDistanceSqr(pos, hero.pos) < Range and IsValid(hero) then
 		count = count + 1
@@ -25,7 +18,7 @@ end
 function LoadScript()
 
 	Menu = MenuElement({type = MENU, id = "PussyAIO".. myHero.charName, name = myHero.charName})
-	Menu:MenuElement({name = " ", drop = {"Version 0.03"}})	
+	Menu:MenuElement({name = " ", drop = {"Version 0.04"}})	
 	
 	Menu:MenuElement({id = "Key", name = "Key Settings", type = MENU})
 	Menu.Key:MenuElement({id = "Combo",name = "Combo Key", key = string.byte(" ")})
@@ -46,6 +39,7 @@ function LoadScript()
 	Menu.Rset:MenuElement({id = "AllyHp", name = "Use Ult if AllyHP lower than ",value = 30, min = 1, max = 100,step = 1, identifier = "%"})	
 	
 	Menu:MenuElement({type = MENU, id = "Pred", name = "Prediction"})
+	Menu.Pred:MenuElement({id = "Change", name = "Change Prediction Typ", value = 1, drop = {"Gamsteron Prediction", "Premium Prediction"}})	
 	Menu.Pred:MenuElement({id = "PredQ", name = "Hitchance[Q]", value = 1, drop = {"Normal", "High", "Immobile"}})	
 
 	Menu:MenuElement({type = MENU, id = "Draw", name = "Drawings"})
@@ -64,6 +58,8 @@ function LoadScript()
 	Type = _G.SPELLTYPE_CIRCLE, Delay = 0.25, Radius = 235, Range = 825, Speed = 1000, Collision = false
 	}
 	
+	QspellData = {speed = 1000, range = 825, delay = 0.25, radius = 235, collision = {}, type = "circle"}	
+	
     if _G.EOWLoaded then
 		Orb = 1
 	elseif _G.SDK and _G.SDK.Orbwalker then
@@ -76,23 +72,19 @@ function LoadScript()
 	Callback.Add("Tick", function() Tick() end)	
 
 	Callback.Add("Draw", function()
-		local textPos = myHero.pos:To2D()	
-		if not FileExist(COMMON_PATH .. "GamsteronPrediction.lua") then
-			Draw.Text("GsoPred. installed Press 2x F6", 50, textPos.x + 100, textPos.y - 250, Draw.Color(255, 255, 0, 0))
-		end	
 		if myHero.dead then return end
 
 		if Menu.Draw.Q:Value() then
-			local qcolor = Ready(_Q) and  Draw.Color(189, 183, 107, 255) or Draw.Color(240,255,0,0)
-			Draw.Circle(Vector(myHero.pos),Q.range,1,qcolor)
+			local qcolor = Ready(_Q) and  DrawColor(189, 183, 107, 255) or DrawColor(240,255,0,0)
+			DrawCircle(Vector(myHero.pos),Q.range,1,qcolor)
 		end
 		if Menu.Draw.W:Value() then
-			local wcolor = Ready(_W) and  Draw.Color(240,30,144,255) or Draw.Color(240,255,0,0)
-			Draw.Circle(Vector(myHero.pos),W.range,1,wcolor)
+			local wcolor = Ready(_W) and  DrawColor(240,30,144,255) or DrawColor(240,255,0,0)
+			DrawCircle(Vector(myHero.pos),W.range,1,wcolor)
 		end
 		if Menu.Draw.E:Value() then
-			local ecolor = Ready(_E) and  Draw.Color(233, 150, 122, 255) or Draw.Color(240,255,0,0)
-			Draw.Circle(Vector(myHero.pos),E.range,1,ecolor)
+			local ecolor = Ready(_E) and  DrawColor(233, 150, 122, 255) or DrawColor(240,255,0,0)
+			DrawCircle(Vector(myHero.pos),E.range,1,ecolor)
 		end
 	end)	
 end
@@ -150,7 +142,7 @@ function AutoW()
 	for i, ally in pairs(GetAllyHeroes()) do
 		if myHero.pos:DistanceTo(ally.pos) <= W.range and IsValid(ally) and Menu.Wset.AutoW:Value() then
 			if ally.health/ally.maxHealth  < Menu.Wset.AllyHp:Value()/100 then
-				Control.CastSpell(HK_W,ally.pos)
+				ControlCastSpell(HK_W,ally.pos)
 				return
 			
 			end			
@@ -160,7 +152,7 @@ end
 
 function AutoW2()
 	if myHero.health/myHero.maxHealth < Menu.Wset.MyHp:Value()/100 and Menu.Wset.AutoW:Value() then
-		Control.CastSpell(HK_W,myHero.pos)
+		ControlCastSpell(HK_W,myHero.pos)
 		return
 	end
 end
@@ -171,7 +163,7 @@ if target == nil then return end
 	if IsValid(target) and myHero.pos:DistanceTo(target.pos) < 900 and Menu.Rset.AutoR:Value() then		
 		for i, ally in pairs(GetAllyHeroes()) do
 			if ally.health/ally.maxHealth < Menu.Rset.AllyHp:Value()/100 and CountEnemiesNear(ally, 800) > 0 then
-				Control.CastSpell(HK_R,target.pos)
+				ControlCastSpell(HK_R,target.pos)
 				return
 			end	
 		end
@@ -183,15 +175,22 @@ local target = GetTarget(1000)
 if target == nil then return end
 	if IsValid(target) and myHero.pos:DistanceTo(target.pos) < 900 and Menu.Rset.AutoR:Value() then	
 		if CountEnemiesNear(target, 500) >= Menu.Rset.RHit:Value() then
-			Control.CastSpell(HK_R,target.pos)
+			ControlCastSpell(HK_R,target.pos)
 			return
 		end	
 	end
 end
 
 function CastQ(target)
-local pred = GetGamsteronPrediction(target, QData, myHero)
-	if pred.Hitchance >= Menu.Pred.PredQ:Value() + 1 then
-		Control.CastSpell(HK_Q,pred.CastPosition)
+	if Menu.Pred.Change:Value() == 1 then
+		local pred = GetGamsteronPrediction(target, QData, myHero)
+		if pred.Hitchance >= Menu.Pred.PredQ:Value()+1 then
+			ControlCastSpell(HK_Q, pred.CastPosition)
+		end
+	else
+		local pred = _G.PremiumPrediction:GetPrediction(myHero, target, QspellData)
+		if pred.CastPos and ConvertToHitChance(Menu.Pred.PredQ:Value(), pred.HitChance) then
+			ControlCastSpell(HK_Q, pred.CastPos)
+		end	
 	end
 end
