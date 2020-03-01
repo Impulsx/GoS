ec38067a6b05c3d8b1f04eee03c1ef80e2b201f1
Pussykate <35004487+Pussykate@users.noreply.github.com>
2020-03-01 13:08:06 +0100

    Update newVersion.lua

--- AIOChampions/newVersion.lua
+++ AIOChampions/newVersion.lua
@@ -1,7 +1,7 @@
 Data = {
 
     Loader = {
-        Version = 0.07,
+        Version = 0.08,
     },
     Core = {
         Changelog = "Auto Version ChangeLog Here",
@@ -127,7 +127,7 @@ Data = {
         },
          Sett = {
             Changelog = "Sett Changelog Here",
-            Version = 0.01,
+            Version = 0.02,
         },          
     },
 }
