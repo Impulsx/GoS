309f2ef241028ee90f41e979d300d95eb191d5d8
Pussykate <35004487+Pussykate@users.noreply.github.com>
2020-03-07 17:44:37 +0100

    Update Neeko.lua

--- AIOChampions/Champions/Neeko.lua
+++ AIOChampions/Champions/Neeko.lua
@@ -96,7 +96,7 @@ end
 function LoadScript()
 	HPred()
 	Menu = MenuElement({type = MENU, id = "PussyAIO".. myHero.charName, name = myHero.charName})
-	Menu:MenuElement({name = " ", drop = {"Version 0.04"}})	
+	Menu:MenuElement({name = " ", drop = {"Version 0.05"}})	
 	
 	--AutoE
 	Menu:MenuElement({type = MENU, id = "AutoE", name = "AutoE"})	
@@ -186,6 +186,8 @@ function LoadScript()
 		Orb = 3
 	elseif _G.gsoSDK then
 		Orb = 4
+	elseif _G.PremiumOrbwalker then
+		Orb = 5		
 	end	
 	Callback.Add("Tick", function() Tick() end)
 	
