51f002dc0f66e1d86d5a716007bf4abb227d903e
Pussykate <35004487+Pussykate@users.noreply.github.com>
2020-03-07 17:49:22 +0100

    Update XinZhao.lua

--- AIOChampions/Champions/XinZhao.lua
+++ AIOChampions/Champions/XinZhao.lua
@@ -24,7 +24,7 @@ end
 function LoadScript()
 	
 	Menu = MenuElement({type = MENU, id = "PussyAIO".. myHero.charName, name = myHero.charName})
-	Menu:MenuElement({name = " ", drop = {"Version 0.04"}})	
+	Menu:MenuElement({name = " ", drop = {"Version 0.05"}})	
 	Menu:MenuElement({type = MENU, id = "Mode", name = myHero.charName})
 	
 	--Main Menu-- PussyXinZhao -- AutoW
@@ -79,6 +79,8 @@ function LoadScript()
 		Orb = 3
 	elseif _G.gsoSDK then
 		Orb = 4
+	elseif _G.PremiumOrbwalker then
+		Orb = 5		
 	end	
 	Callback.Add("Tick", function() Tick() end)
 	
