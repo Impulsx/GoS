80987a09a8a30b30717eb25430a850616db9e25f
Pussykate <35004487+Pussykate@users.noreply.github.com>
2020-03-07 17:42:44 +0100

    Update Malzahar.lua

--- AIOChampions/Champions/Malzahar.lua
+++ AIOChampions/Champions/Malzahar.lua
@@ -43,7 +43,7 @@ end
 
 function LoadScript()
 	Menu = MenuElement({type = MENU, id = "PussyAIO".. myHero.charName, name = myHero.charName})
-	Menu:MenuElement({name = " ", drop = {"Version 0.05"}})	
+	Menu:MenuElement({name = " ", drop = {"Version 0.06"}})	
 
 	--AutoQ
 	Menu:MenuElement({type = MENU, id = "AutoQ", name = "Auto[Q]Immobile"})
@@ -123,6 +123,8 @@ function LoadScript()
 		Orb = 3
 	elseif _G.gsoSDK then
 		Orb = 4
+	elseif _G.PremiumOrbwalker then
+		Orb = 5		
 	end	
 	Callback.Add("Tick", function() Tick() end)
 
