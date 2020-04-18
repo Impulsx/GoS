584843c680ae29463517d3497338e0eaed4740b0
Pussykate <35004487+Pussykate@users.noreply.github.com>
2020-04-18 11:42:19 +0200

    Update Renekton.lua

--- AIOChampions/Champions/Renekton.lua
+++ AIOChampions/Champions/Renekton.lua
@@ -38,24 +38,26 @@ local function GetEnemyCount(range, pos)
 	return count
 end
 
-local LastPos = {}
-local function MyHeroLastPos()
-	return LastPos
-end 
+local LastPos = nil
+local iDmg = 0
+local qDmg = 0
+local wDmg = 0
+local eDmg = 0
+
 
 function LoadScript()
 	
 	Menu = MenuElement({type = MENU, id = "PussyAIO".. myHero.charName, name = myHero.charName})
-	Menu:MenuElement({name = " ", drop = {"Version 0.01"}})		
+	Menu:MenuElement({name = " ", drop = {"Version 0.02"}})		
 	
 	--Combo  
 	Menu:MenuElement({type = MENU, id = "Combo", name = "Combo"})	
 	Menu.Combo:MenuElement({id = "UseQ", name = "[Q]", value = true})	
-	Menu.Combo:MenuElement({id = "UseR", name = "[W]", value = true})	
+	Menu.Combo:MenuElement({id = "UseW", name = "[W]", value = true})	
 	Menu.Combo:MenuElement({id = "UseE", name = "[E]", value = true})
 
 	--Ult  
-	Menu.Combo:MenuElement({type = MENU, id = "ult", name = "Ultimate Settings"})	
+	Menu.Combo:MenuElement({type = MENU, id = "ult", name = "Auto Ultimate Settings"})	
 	Menu.Combo.ult:MenuElement({id = "R", name = "Save Life", value = true})		
 	Menu.Combo.ult:MenuElement({id = "Rhp", name = "Use[R] if Renekton Hp lower than", value = 20, min = 0, max = 100, identifier = "%"})
 	Menu.Combo.ult:MenuElement({name = " ", drop = {"-------------------------------"}})	
@@ -97,6 +99,7 @@ function LoadScript()
 	Menu.Drawing:MenuElement({id = "DrawQ", name = "Draw [Q] Range", value = false})
 	Menu.Drawing:MenuElement({id = "DrawE", name = "Draw [E] Range", value = false})
 	Menu.Drawing:MenuElement({id = "DrawR", name = "Draw [R] Range", value = false})
+	Menu.Drawing:MenuElement({id = "text", name = "Draw Kill Text", value = true})	
 	
 	
 	EData =
@@ -122,7 +125,9 @@ function LoadScript()
 	
 	Callback.Add("Draw", function()
 		if myHero.dead then return end
-		
+		if Menu.Drawing.text:Value() then		
+			DamageCalculation()
+		end
 		if Menu.Drawing.DrawR:Value() and Ready(_R) then
 		DrawCircle(myHero, 175, 1, DrawColor(255, 225, 255, 10))
 		end                                                 
@@ -140,29 +145,193 @@ if MyHeroNotReady() then return end
 local Mode = GetMode()
 	if Mode == "Combo" then
 		Combo()
-		Ult()
 	elseif Mode == "Harass" then
 		Harass()
 	elseif Mode == "Clear" then
-		--Clear()
-		--JungleClear()			
+		Clear()
+		JungleClear()			
 	end	
-
+	Ult()
 	KillSteal()
+end
+
+function Combo()
+local target = GetTarget(1100)
+if target == nil then return end 
+	if Menu.Combo.UseQ:Value() then 
+		CastQ(target) 
+	end
+	if Menu.Combo.UseW:Value() then 
+		CastW(target) 
+	end
+	if Menu.Combo.UseE:Value() then 
+		Slice(target)  
+		Dice(target) 
+	end
+end
+
+function Ult()
+if not Ready(_R) then return end
+	if Menu.Combo.ult.R:Value() then 
+		if Ready(_R) and myHero.health/myHero.maxHealth <= Menu.Combo.ult.Rhp:Value() / 100 then 
+			ControlCastSpell(HK_R)
+		end
+	end
+	if Menu.Combo.ult.tower:Value() then
+		local count = GetEnemyCount(1000, myHero)
+		if IsUnderTurret(myHero) and Ready(_R) and myHero.health/myHero.maxHealth <= Menu.Combo.ult.towerhp:Value() / 100 and count > 0 then 
+			ControlCastSpell(HK_R)
+		end
+	end	
+	if Menu.Combo.ult.count:Value() then
+		local count = GetEnemyCount(Menu.Combo.ult.countRange:Value(), myHero)	
+		if Ready(_R) and count >= Menu.Combo.ult.countR:Value() and myHero.health/myHero.maxHealth <= Menu.Combo.ult.counthp:Value() / 100 then
+			ControlCastSpell(HK_R)
+		end
+	end
+end
+
+function Harass()
+local target = GetTarget(1100)
+if target == nil then return end
+	
+	if Menu.Harass.harassMode:Value() == 1 then  
+		CastQ(target) 
+	end
+	if Menu.Harass.harassMode:Value() == 2 then 
+		CastQ(target)  
+		CastW(target) 
+	end
+	if Menu.Harass.harassMode:Value() == 3 then
+ 		CastQ(target) 
+		CastW(target)  
+		Slice(target)  
+		Dice(target) 
+	end
+	if Menu.Harass.harassMode:Value() == 4 then
+		local E2Buff = HasBuff(myHero, "renektonsliceanddicedelay")
+		if not E2Buff then
+			if myHero.pos:DistanceTo(target.pos) <= 450 and IsValid(target) then
+				LastPos = myHero.pos
+			end
+			Slice(target) 
+		end	
+		if E2Buff then 
+			CastQ(target) 
+		end
+		if E2Buff and not Ready(_Q) then 
+			CastW(target) 
+		end
+		if E2Buff and not Ready(_Q) and not Ready(_W) then 
+			LastDash() 
+		end
+	end				
+end
+
+function LastDash()
+	if LastPos then
+		ControlCastSpell(HK_E, LastPos)
+	end	
+end
+
+function KillSteal()
+	for i, enemy in pairs(GetEnemyHeroes()) do
+		if myHero.pos:DistanceTo(enemy.pos) <= 1000 and IsValid(enemy) then 
+		local distance = myHero.pos:DistanceTo(enemy.pos)
+		local hp = enemy.health
+		local q1Dmg = getdmg("Q", enemy, myHero, 1)
+		local w1Dmg = getdmg("W", enemy, myHero, 1)
+		local e1Dmg = getdmg("E", enemy, myHero, 1)		
+		local q2Dmg = getdmg("Q", enemy, myHero, 2)
+		local w2Dmg = getdmg("W", enemy, myHero, 2)
+		local e2Dmg = getdmg("E", enemy, myHero, 2)
+			if Menu.ks.smartKS:Value() then 
+				if myHero.mana/myHero.maxMana >= 0.5 then 					
+					if hp <= q2Dmg and Ready(_Q) and distance <= 325 then 						
+						CastQ(enemy)
+					elseif hp <= w2Dmg and Ready(_W) and distance <= 325 then 
+						CastW(enemy)
+					elseif hp <= e2Dmg and Ready(_E) and distance <= 450 then 
+						AimE(enemy)
+					elseif hp <= (q2Dmg + w1Dmg) and Ready(_Q) and Ready(_W) and distance <= 325 then 
+						CastQ(enemy)						
+					elseif hp <= (q1Dmg + w2Dmg) and Ready(_Q) and Ready(_W) and distance <= 325 then 
+						CastW(enemy)
+					elseif hp <= (q1Dmg + e2Dmg) and Ready(_Q) and Ready(_E) and distance <= 450 then 
+						AimE(enemy)
+					elseif hp <= (w1Dmg + e2Dmg) and Ready(_W) and Ready(_E) and distance <= 450 then 
+						AimE(enemy)
+					elseif hp <= (q1Dmg + w1Dmg + e2Dmg) and Ready(_Q) and Ready(_W) and Ready(_E) and distance <= 450 then 
+						AimE(enemy)
+					end
+				else
+					if hp <= q1Dmg and Ready(_Q) and distance <= 325 then 
+						CastQ(enemy)
+					elseif hp <= w1Dmg and Ready(_W) and distance <= 325 then 
+						CastW(enemy)
+					elseif hp <= e1Dmg and Ready(_E) and distance <= 450 then 
+						AimE(enemy)
+					elseif hp <= (q1Dmg + w1Dmg) and Ready(_Q) and Ready(_W) and distance <= 325 then 
+						CastW(enemy)
+					elseif hp <= (q1Dmg + e1Dmg) and Ready(_Q) and Ready(_E) and distance <= 450 then 
+						AimE(enemy)
+					elseif hp <= (w1Dmg + e1Dmg) and Ready(_W) and Ready(_E) and distance <= 450 then 
+						AimE(enemy)
+					elseif hp <= (q1Dmg + w1Dmg + e1Dmg) and Ready(_Q) and Ready(_W) and Ready(_E) and distance <= 450 then 
+						AimE(enemy)
+					end
+				end	
+			end	
+		end
+	end
+end
+
+function JungleClear()
+    for i = 1, GameMinionCount() do
+    local minion = GameMinion(i)
+        if minion.team == TEAM_JUNGLE and myHero.pos:DistanceTo(minion.pos) < 500 then
+			if Menu.JClear.UseQ:Value() then 
+				CastQ(minion) 
+			end
+			if Menu.JClear.UseW:Value() then 
+				CastW(minion) 
+			end
+			if Menu.JClear.UseE:Value() and Ready(_E) and myHero.pos:DistanceTo(minion.pos) < 450 and IsValid(minion) then
+				ControlCastSpell(HK_E, minion.pos)
+			end	
+		end
+	end	
+end
+
+function Clear()
+    for i = 1, GameMinionCount() do
+    local minion = GameMinion(i)
+        if minion.team == TEAM_ENEMY and myHero.pos:DistanceTo(minion.pos) < 500 then
+			if Menu.Clear.UseQ:Value() then 
+				CastQ(minion) 
+			end
+			if Menu.Clear.UseW:Value() then 
+				CastW(minion) 
+			end
+			if Menu.Clear.UseE:Value() and Ready(_E) and myHero.pos:DistanceTo(minion.pos) < 450 and IsValid(minion) then
+				ControlCastSpell(HK_E, minion.pos)
+			end	
+		end
+	end
 end
 
 function CastQ(unit)
 	if Ready(_Q) and myHero.pos:DistanceTo(unit.pos) < 325 then
 		if IsValid(unit) then 
-			ControlCastSpell(_Q)
+			ControlCastSpell(HK_Q)
 		end
 	end
 end
 
 function CastW(unit)
-	if Ready(_W) and myHero.pos:DistanceTo(unit.pos) < (myHero.range + 50) then
+	if Ready(_W) and myHero.pos:DistanceTo(unit.pos) < 325 then
 		if IsValid(unit) then 
-			ControlCastSpell(_W, unit)
+			ControlCastSpell(HK_W, unit)
 		end
 	end
 end
@@ -184,7 +353,9 @@ function AimE(unit)
 end
 
 function Slice(unit)
-	if myHero.pos:DistanceTo(unit.pos) < 450 and Ready(_E) and myHero:GetSpellData(_E).name == "RenektonSliceAndDice" and IsValid(unit) then
+local E2Buff = HasBuff(myHero, "renektonsliceanddicedelay")
+if E2Buff then return end	
+	if myHero.pos:DistanceTo(unit.pos) < 450 and Ready(_E) and IsValid(unit) then
 		if Menu.Pred.Change:Value() == 1 then
 			local pred = GetGamsteronPrediction(unit, EData, myHero)
 			if pred.Hitchance >= Menu.Pred.PredE:Value()+1 then
@@ -200,7 +371,8 @@ function Slice(unit)
 end
 
 function Dice(unit)
-	if myHero.pos:DistanceTo(unit.pos) < 450 and Ready(_E) and myHero:GetSpellData(_E).name == "renektondice" and IsValid(unit) then
+local E2Buff = HasBuff(myHero, "renektonsliceanddicedelay")
+	if myHero.pos:DistanceTo(unit.pos) < 450 and Ready(_E) and E2Buff and IsValid(unit) then
 		if Menu.Pred.Change:Value() == 1 then
 			local pred = GetGamsteronPrediction(unit, EData, myHero)
 			if pred.Hitchance >= Menu.Pred.PredE:Value()+1 then
@@ -215,96 +387,66 @@ function Dice(unit)
 	end
 end
 
-function Ult()
-if not Ready(_R) then return end
-	if Menu.Combo.ult.R:Value() then 
-		if Ready(_R) and myHero.health/myHero.maxHealth <= Menu.Combo.ult.Rhp:Value() / 100 then 
-			ControlCastSpell(_R)
-		end
-	end
-	if Menu.Combo.ult.tower:Value() then
+function DamageCalculation()
+local enemy = GetTarget(1500)
+if enemy == nil then return end
+	if myHero.pos:DistanceTo(enemy.pos) <= 1000 and IsValid(enemy) then
+		local qDmg 	= getdmg("Q", enemy, myHero, 1)	
+		local wDmg	= getdmg("W", enemy, myHero, 1)	
+		local eDmg	= getdmg("E", enemy, myHero, 1)
 
-		if IsUnderTurret(myHero) and Ready(_R) and myHero.health/myHero.maxHealth <= Menu.Combo.ult.towerhp:Value() / 100 and count > 0 then 
-			ControlCastSpell(_R)
-		end
-	end	
-	if Menu.Combo.ult.count:Value() then
-		local count = GetEnemyCount(Menu.Combo.ult.countRange:Value(), myHero)	
-		if Ready(_R) and count >= Menu.Combo.ult.countR:Value() and myHero.health/myHero.maxHealth <= Menu.Combo.ult.counthp:Value() / 100 then
-			ControlCastSpell(_R)
-		end
-	end
-end
-
-function Harass()
-local target = GetTarget(1100)
-if target == nil then return end
-	
-	if Menu.Harass.harassMode:Value() == 1 then  
-		CastQ(target) 
-	
-	elseif Menu.Harass.harassMode:Value() == 2 then 
-		CastQ(target)  
-		CastW(target) 
-	
-	elseif Menu.Harass.harassMode:Value() == 3 then
- 		CastQ(target) 
-		CastW(target)  
-		Slice(target)  
-		Dice(target) 
-	
-	elseif Menu.Harass.harassMode:Value() == 4 then
-		SavePos(target)
-		Slice(target) 
-		if myHero:GetSpellData(_E).name == "renektondice" then 
-			CastQ(target) 
-		end
-		if myHero:GetSpellData(_E).name == "renektondice" and not Ready(_Q) then 
-			CastW(target) 
-		end
-		if myHero:GetSpellData(_E).name == "renektondice" and not Ready(_Q) and not Ready(_W) then 
-			LastDash() 
-		end
-	end				
-end
-
-function SavePos(unit)
-	if myHero.pos:DistanceTo(unit.pos) <= 450 and IsValid(unit) then
-		TableInsert(LastPos, myHero,pos)
-	end
-end
-
-function LastDash()
-	for i, Pos in pairs(MyHeroLastPos()) do
-		if Pos then
-			ControlCastSpell(_E, Pos)
-		end
-	end	
-end
-
-function KillSteal()
-	for i, enemy in pairs(GetEnemyHeroes()) do
-		if enemy ~= nil and myHero.pos:DistanceTo(enemy.pos) <= 1000 and IsValid(enemy) then
-		local distance = myHero.pos:DistanceTo(enemy.pos)
-		local hp = enemy.health
-		local qDmg = getdmg("Q", enemy, myHero)
-		local wDmg = getdmg("W", enemy, myHero)
-		local eDmg = getdmg("E", enemy, myHero)
-			if hp <= qDmg and Ready(_Q) and (distance <= 325)
-				then CastQ(enemy)
-			elseif hp <= wDmg and Ready(_W) and (distance <= (myHero.range + 50)) 
-				then CastW(enemy)
-			elseif hp <= eDmg and Ready(_E) and (distance <= 450) 
-				then AimE()
-			elseif hp <= (qDmg + wDmg) and Ready(_Q) and Ready(_W) and (distance <= 325)
-				then CastW(enemy)
-			elseif hp <= (qDmg + eDmg) and Ready(_Q) and Ready(_E) and (distance <= 325)
-				then AimE()
-			elseif hp <= (wDmg + eDmg) and Ready(_W) and Ready(_E) and (distance <= (myHero.range + 50))
-				then AimE()
-			elseif hp <= (qDmg + wDmg + eDmg) and Ready(_Q) and Ready(_W) and Ready(_E) and (distance <= 325)
-				then AimE()
+		-- "Kill! - (Q)" --
+		if enemy.health <= qDmg then
+			if Ready(_Q) then 
+				DrawText("Kill! - (Q)", 16, enemy.pos2D.x - 50, enemy.pos2D.y + 50,DrawColor(255, 255, 0, 0))
+			else 
+				DrawText("Wait for CD's!", 16, enemy.pos2D.x - 50, enemy.pos2D.y + 50,DrawColor(255, 0, 255, 0))
 			end
+		--	"Kill! - (W)" --
+		elseif enemy.health <= wDmg then
+			if Ready(_W) then 
+				DrawText("Kill! - (W)", 16, enemy.pos2D.x - 50, enemy.pos2D.y + 50,DrawColor(255, 255, 0, 0))
+			else 
+				DrawText("Wait for CD's!", 16, enemy.pos2D.x - 50, enemy.pos2D.y + 50,DrawColor(255, 0, 255, 0))
+			end
+		-- "Kill! - (E)" --
+		elseif enemy.health <= eDmg then
+			if Ready(_E) then 
+				DrawText("Kill! - (E)", 16, enemy.pos2D.x - 50, enemy.pos2D.y + 50,DrawColor(255, 255, 0, 0))
+			else 
+				DrawText("Wait for CD's!", 16, enemy.pos2D.x - 50, enemy.pos2D.y + 50,DrawColor(255, 0, 255, 0))
+			end
+		-- "Kill! - (Q)+(W)" --
+		elseif enemy.health <= qDmg+wDmg then
+			if Ready(_Q) and Ready(_W) then 
+				DrawText("Kill! - (Q)+(W)", 16, enemy.pos2D.x - 50, enemy.pos2D.y + 50,DrawColor(255, 255, 0, 0))
+			else 
+				DrawText("Wait for CD's!", 16, enemy.pos2D.x - 50, enemy.pos2D.y + 50,DrawColor(255, 0, 255, 0))
+			end
+		-- "Kill! - (Q)+(E)" --
+		elseif enemy.health <= qDmg+eDmg then
+			if Ready(_Q) and Ready(_E) then 
+				DrawText("Kill! - (Q)+(E)", 16, enemy.pos2D.x - 50, enemy.pos2D.y + 50,DrawColor(255, 255, 0, 0))
+			else 
+				DrawText("Wait for CD's!", 16, enemy.pos2D.x - 50, enemy.pos2D.y + 50,DrawColor(255, 0, 255, 0))
+			end
+		-- "Kill! - (W)+(E)" --
+		elseif enemy.health <= wDmg+eDmg then
+			if Ready(_W) and Ready(_E) then 
+				DrawText("Kill! - (W)+(E)", 16, enemy.pos2D.x - 50, enemy.pos2D.y + 50,DrawColor(255, 255, 0, 0))
+			else 
+				DrawText("Wait for CD's!", 16, enemy.pos2D.x - 50, enemy.pos2D.y + 50,DrawColor(255, 0, 255, 0))
+			end
+		-- "Kill! - (Q)+(W)+(E)" --
+		elseif enemy.health <= qDmg+wDmg+eDmg then
+			if Ready(_Q) and Ready(_W) and Ready(_E) then 
+				DrawText("Kill! - (Q)+(W)+(E)", 16, enemy.pos2D.x - 50, enemy.pos2D.y + 50,DrawColor(255, 255, 0, 0))
+			else 
+				DrawText("Wait for CD's!", 16, enemy.pos2D.x - 50, enemy.pos2D.y + 50,DrawColor(255, 0, 255, 0))
+			end
+		-- "Harass your enemy!" -- 
+		else 
+			DrawText("Harass him!", 16, enemy.pos2D.x - 50, enemy.pos2D.y + 50,DrawColor(255, 0, 255, 0))			
 		end
 	end
 end
