207cb085b60f1b08641302f25f0d285a7624831d
Pussykate <35004487+Pussykate@users.noreply.github.com>
2020-04-18 19:52:10 +0200

    Update PussyAIONewGen.lua

--- PussyAIONewGen.lua
+++ PussyAIONewGen.lua
@@ -145,15 +145,86 @@ local function AutoUpdate()
 		
 	end
 	
+	local function Info()
+		Menu = MenuElement({type = MENU, id = "PussyAIOInfo", name = "PussyAio Champion Info"})
+		Menu:MenuElement({id = "info", name = "Draw Supported Champions", value = true})		
+	end
+	
 	local function DrawInfo() 
-	if Game.Timer() > 20 then return end 
-		Draw.Text(myHero.charName.." loads in game after 30 sec", 24, myHero.pos2D.x, myHero.pos2D.y,Draw.Color(0xFF00FF00))	
+		if Menu.info and Menu.info:Value() then
+			local textPos = myHero.pos:To2D()
+			Draw.Rect(textPos.x -500, textPos.y - 300, 1200, 500, Draw.Color(30, 255, 0, 0)) 	
+			Draw.Rect(textPos.x -500, textPos.y - 300, 1220, 50, Draw.Color(30, 0, 255, 0)) 	--upper
+			Draw.Rect(textPos.x -500, textPos.y + 200, 1200, 20, Draw.Color(30, 0, 255, 0))		--under
+			Draw.Rect(textPos.x -500, textPos.y - 250, 20, 450, Draw.Color(30, 0, 255, 0)) 		-- left
+			Draw.Rect(textPos.x +700, textPos.y - 250, 20, 470, Draw.Color(30, 0, 255, 0))		-- right			
+			Draw.Text("PUSSY AIO", 64, textPos.x - 30, textPos.y - 309, Draw.Color(40, 225, 255, 255))			
+			--ADC--
+			Draw.Text("---ADC---", 30, textPos.x -350, textPos.y - 250, Draw.Color(140, 0, 255, 0))
+			Draw.Rect(textPos.x -359, textPos.y - 220, 120, 2, Draw.Color(140, 0, 255, 0))
+			Draw.Text("Caitlyn", 20, textPos.x - 323, textPos.y - 200, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Jhin", 20, textPos.x - 323, textPos.y - 180, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Kalista", 20, textPos.x - 323, textPos.y - 160, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Kaisa", 20, textPos.x - 323, textPos.y - 140, Draw.Color(140, 225, 255, 0))	
+			Draw.Text("Tristana", 20, textPos.x - 323, textPos.y - 120, Draw.Color(140, 225, 255, 0))			
+			
+			--Mid--
+			Draw.Text("---MID---", 30, textPos.x -150, textPos.y - 250, Draw.Color(140, 0, 255, 0))
+			Draw.Rect(textPos.x -158, textPos.y - 220, 120, 2, Draw.Color(140, 0, 255, 0))
+			Draw.Text("Ahri", 20, textPos.x - 123, textPos.y - 200, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Cassiopeia", 20, textPos.x - 123, textPos.y - 180, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Ekko", 20, textPos.x - 123, textPos.y - 160, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Kassadin", 20, textPos.x - 123, textPos.y - 140, Draw.Color(140, 225, 255, 0))	
+			Draw.Text("Lux", 20, textPos.x - 123, textPos.y - 120, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Malzahar", 20, textPos.x - 123, textPos.y - 100, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Neeko", 20, textPos.x - 123, textPos.y - 80, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Ryze", 20, textPos.x - 123, textPos.y - 60, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Veigar", 20, textPos.x - 123, textPos.y - 40, Draw.Color(140, 225, 255, 0))	
+			Draw.Text("Zoe", 20, textPos.x - 123, textPos.y - 20, Draw.Color(140, 225, 255, 0))			
+
+			--TOP--
+			Draw.Text("---TOP---", 30, textPos.x +50, textPos.y - 250, Draw.Color(140, 0, 255, 0))
+			Draw.Rect(textPos.x +41, textPos.y - 220, 120, 2, Draw.Color(140, 0, 255, 0))
+			Draw.Text("Akali", 20, textPos.x +80, textPos.y - 200, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Camille", 20, textPos.x +80, textPos.y - 180, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Chogath", 20, textPos.x +80, textPos.y - 160, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Diana", 20, textPos.x +80, textPos.y - 140, Draw.Color(140, 225, 255, 0))	
+			Draw.Text("Fiora", 20, textPos.x +80, textPos.y - 120, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Garen", 20, textPos.x +80, textPos.y - 100, Draw.Color(140, 225, 255, 0))	
+			Draw.Text("Mordekaiser", 20, textPos.x +80, textPos.y - 80, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Qiyana", 20, textPos.x +80, textPos.y - 60, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Renekton", 20, textPos.x +80, textPos.y - 40, Draw.Color(140, 225, 255, 0))	
+			Draw.Text("Sylas", 20, textPos.x +80, textPos.y - 20, Draw.Color(140, 225, 255, 0))			
+
+			--Supp--
+			Draw.Text("---SUP---", 30, textPos.x +250, textPos.y - 250, Draw.Color(140, 0, 255, 0))
+			Draw.Rect(textPos.x +240, textPos.y - 220, 120, 2, Draw.Color(140, 0, 255, 0))
+			Draw.Text("Morgana", 20, textPos.x +280, textPos.y - 200, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Pyke", 20, textPos.x +280, textPos.y - 180, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Soraka", 20, textPos.x +280, textPos.y - 160, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Sona", 20, textPos.x +280, textPos.y - 140, Draw.Color(140, 225, 255, 0))	
+			Draw.Text("Zyra", 20, textPos.x +280, textPos.y - 120, Draw.Color(140, 225, 255, 0))			
+
+			--Jungle--
+			Draw.Text("---JGL---", 30, textPos.x +450, textPos.y - 250, Draw.Color(140, 0, 255, 0))
+			Draw.Rect(textPos.x +440, textPos.y - 220, 120, 2, Draw.Color(140, 0, 255, 0))
+			Draw.Text("Nocturne", 20, textPos.x +480, textPos.y - 200, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Rengar", 20, textPos.x +480, textPos.y - 180, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Sett", 20, textPos.x +480, textPos.y - 160, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Warwick", 20, textPos.x +480, textPos.y - 140, Draw.Color(140, 225, 255, 0))	
+			Draw.Text("XinZhao", 20, textPos.x +480, textPos.y - 120, Draw.Color(140, 225, 255, 0))
+			Draw.Text("Nidalee", 20, textPos.x +480, textPos.y - 100, Draw.Color(140, 225, 255, 0))			
+		end
+	
+		if Game.Timer() > 30 then return end 
+		Draw.Text(myHero.charName.." loads in game after 30 sec", 24, myHero.pos2D.x - 50, myHero.pos2D.y + 195, Draw.Color(0xFF00FF00))	
 	end	
 	
 	local function LoadScript()
 		if CheckSupported() then
 			InitializeScript()
 			LoadLibs()
+			Info()
 			Callback.Add("Draw", function() DrawInfo() end)
 		else
 			print("PussyAIO: Champion not supported: ".. myHero.charName)
