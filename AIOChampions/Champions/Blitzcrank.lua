29014c0eb5c7e204b39b41e71455ddb3814e7ec6
Pussykate <35004487+Pussykate@users.noreply.github.com>
2020-05-21 10:01:47 +0200

    Update Blitzcrank.lua

--- AIOChampions/Champions/Blitzcrank.lua
+++ AIOChampions/Champions/Blitzcrank.lua
@@ -1,3 +1,7 @@
+local function GetEnemyHeroes()
+	return Enemies
+end
+
 local function GetEnemyCount(range, pos)
     local pos = pos.pos
 	local count = 0
@@ -11,53 +15,33 @@ local function GetEnemyCount(range, pos)
 	return count
 end
 
-local function IsNearAllyTurret(unit)
+local function IsAllyTurret(unit)
     for i = 1, GameTurretCount() do
         local turret = GameTurret(i)
-        local range = (turret.boundingRadius + 750 + unit.boundingRadius / 2 + 350)
+        local range = (turret.boundingRadius + 650 + unit.boundingRadius / 2)
         if turret.isAlly and not turret.dead then
             if turret.pos:DistanceTo(unit.pos) < range then
-                return true
-				return turret
+				return true
             end
         end
     end
     return false
 end
 
-local function HasBuff(unit, buffname)
-	for i = 0, unit.buffCount do
-		local buff = unit:GetBuff(i)
-		if buff.name == buffname and buff.count > 0 then 
-			return true
-		end
-	end
-	return false
-end
-
-local function ReadyFlash()
-	if myHero:GetSpellData(SUMMONER_1).name == "SummonerFlash" and Game.CanUseSpell(SUMMONER_1) == 0 then
-		return true
-	elseif myHero:GetSpellData(SUMMONER_2).name == "SummonerFlash" and Game.CanUseSpell(SUMMONER_2) == 0 then
-		return true
-	end
-    return false
-end
-
 function LoadScript()
 	
 	Menu = MenuElement({type = MENU, id = "PussyAIO".. myHero.charName, name = myHero.charName})
-	Menu:MenuElement({name = " ", drop = {"Version 0.01"}})	
+	Menu:MenuElement({name = " ", drop = {"Version 0.02"}})	
 	
-	Menu:MenuElement({type = MENU, id = "Q", name = "Special Hook Mode"})
-	Menu.Q:MenuElement({name = " ", drop = {"Auto [Q] + Flash under Ally Tower"}})		
-	Menu.Q:MenuElement({id = "UseQ", name = "Use Special Hook Mode", value = true})	
-	Menu.Q:MenuElement({id = "DrawQ", name = "Draw Tower Hook Mode range", value = true})	
+	Menu:MenuElement({type = MENU, id = "Q", name = "Auto Q + E"})
+	Menu.Q:MenuElement({name = " ", drop = {"Auto [Q] + [E] under Ally Tower"}})		
+	Menu.Q:MenuElement({id = "UseQ", name = "Use Auto [Q] + [E]", value = true})		
 	
 	--ComboMenu  
 	Menu:MenuElement({type = MENU, id = "Combo", name = "Combo"})
 	Menu.Combo:MenuElement({id = "UseQ", name = "[Q]", value = true})
-	Menu.Combo:MenuElement({id = "Qrange", name = "[Q] if range lower than -->", value = 1050, min = 0, max = 1150})	
+	Menu.Combo:MenuElement({id = "Qrange", name = "[Q] if range lower than -->", value = 1050, min = 0, max = 1150})
+	Menu.Combo:MenuElement({id = "Qrange2", name = "[Q] if range bigger than -->", value = 400, min = 0, max = 1150})	
 	Menu.Combo:MenuElement({id = "UseW", name = "[W] +attack speed in AA range ", value = true})
 	Menu.Combo:MenuElement({id = "UseE", name = "[E]", value = true})	
 	
@@ -70,7 +54,7 @@ function LoadScript()
 
 	--Prediction
 	Menu:MenuElement({type = MENU, id = "Pred", name = "Prediction"})
-	Menu.Pred:MenuElement({id = "Change", name = "Change Prediction Typ", value = 1, drop = {"Gamsteron Prediction", "Premium Prediction"}})	
+	Menu.Pred:MenuElement({id = "Change", name = "Change Prediction Typ", value = 2, drop = {"Gamsteron Prediction", "Premium Prediction"}})	
 	Menu.Pred:MenuElement({id = "PredQ", name = "Hitchance[Q]", value = 1, drop = {"Normal", "High", "Immobile"}})	
 
 	--Drawing 
@@ -81,10 +65,10 @@ function LoadScript()
 
 	QData =
 	{
-	Type = _G.SPELLTYPE_LINE, Delay = 0.25, Radius = 70, Range = 1150, Speed = 1800, Collision = true, MaxCollision = 0, CollisionTypes = { _G.COLLISION_MINION }
+	Type = _G.SPELLTYPE_LINE, Delay = 0.25, Radius = 35, Range = 1150, Speed = 1800, Collision = true, MaxCollision = 0, CollisionTypes = { _G.COLLISION_MINION }
 	}
 	
-	QspellData = {speed = 1800, range = 1150, delay = 0.25, radius = 70, collision = {"minion"}, type = "linear"}	
+	QspellData = {speed = 1800, range = 1150, delay = 0.25, radius = 35, collision = {"minion"}, type = "linear"}	
 
   	                                           
 	if _G.EOWLoaded then
@@ -109,15 +93,6 @@ function LoadScript()
 		end
 		if Menu.Drawing.DrawE:Value() and Ready(_E) then
 		DrawCircle(myHero, 300, 1, DrawColor(225, 225, 125, 10))
-		end
-		if Menu.Q.DrawQ:Value() and Ready(_Q) and ReadyFlash() then
-			for i = 1, GameTurretCount() do
-				local turret = GameTurret(i)
-				if turret.isAlly and not turret.dead then
-					local range = (turret.boundingRadius + 750 + myHero.boundingRadius / 2 + 350)				
-					DrawCircle(turret, range, 1, DrawColor(0xFF00FF00))
-				end
-			end	
 		end		
 	end)		
 end
@@ -128,65 +103,67 @@ local Mode = GetMode()
 	if Mode == "Combo" then
 		Combo()			
 	end
-	if ReadyFlash() then
-		AutoQ()	
-	end	
+	AutoQ()	
 end
 
 function AutoQ()
-local target = GetTarget(1200)     	
-if target == nil then return end	
+	for i, target in ipairs(GetEnemyHeroes()) do    	
+	if myHero.pos:DistanceTo(target.pos) > 1200 then return end	
 	
-	if Ready(_Q) and Menu.Q.UseQ:Value() and IsValid(target) then
-		local Tower = IsNearAllyTurret(myHero)
-		if Tower and myHero.pos:DistanceTo(target.pos) <= Menu.Combo.Qrange:Value() then
-			local buff = HasBuff(target, "rocketgrab2")
-			if buff then
-				if myHero:GetSpellData(SUMMONER_1).name == "SummonerFlash" then
-					ControlCastSpell(HK_SUMMONER_1, Tower.pos)
-				elseif myHero:GetSpellData(SUMMONER_2).name == "SummonerFlash" then
-					ControlCastSpell(HK_SUMMONER_2, Tower.pos)
-				end
-			end
-			
-			if Menu.Pred.Change:Value() == 1 then
-				local pred = GetGamsteronPrediction(target, QData, myHero)
-				if pred.Hitchance >= Menu.Pred.PredQ:Value()+1 then
-					ControlCastSpell(HK_Q, pred.CastPosition)
-				end
-			else
-				local pred = _G.PremiumPrediction:GetPrediction(myHero, target, QspellData)
-				if pred.CastPos and ConvertToHitChance(Menu.Pred.PredQ:Value(), pred.HitChance) then
-					ControlCastSpell(HK_Q, pred.CastPos)
-				end	
-			end
+		if myHero.pos:DistanceTo(target.pos) <= 250 and Ready(_E) then
+			ControlCastSpell(HK_E, target)
 		end	
-	end
+		
+		if Ready(_Q) and Menu.Q.UseQ:Value() and IsValid(target) then
+			local Tower = IsAllyTurret(myHero)
+			if Tower and myHero.pos:DistanceTo(target.pos) <= Menu.Combo.Qrange:Value() then			
+				if Menu.Pred.Change:Value() == 1 then
+					local pred = GetGamsteronPrediction(target, QData, myHero)
+					if pred.Hitchance >= Menu.Pred.PredQ:Value()+1 then
+						SetMovement(false)
+						ControlCastSpell(HK_Q, pred.CastPosition)
+						SetMovement(true)						
+					end
+				else
+					local pred = _G.PremiumPrediction:GetPrediction(myHero, target, QspellData)
+					if pred.CastPos and ConvertToHitChance(Menu.Pred.PredQ:Value(), pred.HitChance) then
+						SetMovement(false)
+						ControlCastSpell(HK_Q, pred.CastPos)
+						SetMovement(true)						
+					end	
+				end
+			end	
+		end
+	end	
 end		
 
 function Combo()
 local target = GetTarget(1200)
 if target == nil then return end
 	if IsValid(target) then
+	
+		if myHero.pos:DistanceTo(target.pos) <= 250 and Menu.Combo.UseE:Value() and Ready(_E) then
+			ControlCastSpell(HK_E, target)
+		end		
 
-		if myHero.pos:DistanceTo(target.pos) <= 300 and Menu.Combo.UseW:Value() and Ready(_W) then
-			ControlCastSpell(HK_W)
-		end	
-
-		if myHero.pos:DistanceTo(target.pos) <= 300 and Menu.Combo.UseE:Value() and Ready(_E) then
-			ControlCastSpell(HK_E)
+		if myHero.pos:DistanceTo(target.pos) <= 250 and Menu.Combo.UseW:Value() and Ready(_W) then
+			ControlCastSpell(HK_W, target)
 		end		
 		
-		if myHero.pos:DistanceTo(target.pos) <= Menu.Combo.Qrange:Value() and Menu.Combo.UseQ:Value() and Ready(_Q) then
+		if myHero.pos:DistanceTo(target.pos) <= Menu.Combo.Qrange:Value() and myHero.pos:DistanceTo(target.pos) > Menu.Combo.Qrange2:Value() and Menu.Combo.UseQ:Value() and Ready(_Q) then
 			if Menu.Pred.Change:Value() == 1 then
 				local pred = GetGamsteronPrediction(target, QData, myHero)
 				if pred.Hitchance >= Menu.Pred.PredQ:Value()+1 then
+					SetMovement(false)				
 					ControlCastSpell(HK_Q, pred.CastPosition)
+					SetMovement(true)					
 				end
 			else
 				local pred = _G.PremiumPrediction:GetPrediction(myHero, target, QspellData)
 				if pred.CastPos and ConvertToHitChance(Menu.Pred.PredQ:Value(), pred.HitChance) then
+					SetMovement(false)				
 					ControlCastSpell(HK_Q, pred.CastPos)
+					SetMovement(true)					
 				end	
 			end
 		end
