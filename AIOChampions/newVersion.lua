532bb7959b5a8a4c731234db967a77c131b8dec7
Pussykate <35004487+Pussykate@users.noreply.github.com>
2020-05-28 21:47:36 +0200

    Update newVersion.lua

--- AIOChampions/newVersion.lua
+++ AIOChampions/newVersion.lua
@@ -175,7 +175,7 @@ Data = {
         },
            Volibear = {
             Changelog = "Volibear Changelog Here",
-            Version = 0.01,
+            Version = 0.02,
         },         
     },
 }
