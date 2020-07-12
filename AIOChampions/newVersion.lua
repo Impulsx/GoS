48fc60d497f245a98bed88c378af4a38d106fa84
Pussykate <35004487+Pussykate@users.noreply.github.com>
2020-07-12 08:03:15 +0200

    Update newVersion.lua

--- AIOChampions/newVersion.lua
+++ AIOChampions/newVersion.lua
@@ -107,7 +107,7 @@ Data = {
         },
          Caitlyn = {
             Changelog = "Caitlyn Changelog Here",
-            Version = 0.12,
+            Version = 0.13,
         },
           Kaisa = {
             Changelog = "Kaisa Changelog Here",
