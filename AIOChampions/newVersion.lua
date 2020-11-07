fdf94b3b3f7da00b4839c46c60a6dc9e4aa70dd2
Pussykate <35004487+Pussykate@users.noreply.github.com>
2020-11-07 21:26:33 +0100

    Update newVersion.lua

--- AIOChampions/newVersion.lua
+++ AIOChampions/newVersion.lua
@@ -211,7 +211,7 @@ Data = {
         }, 
         Illaoi = {
             Changelog = "Illaoi Changelog Here",
-            Version = 0.01,
+            Version = 0.02,
         },        
     },
 }
