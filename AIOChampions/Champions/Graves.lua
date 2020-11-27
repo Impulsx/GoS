d75863269efa817838d95eb70c430a79a2e4fddc
Pussykate <35004487+Pussykate@users.noreply.github.com>
2020-11-27 10:24:02 +0100

    Add files via upload

---
+++ AIOChampions/Champions/Graves.lua
@@ -0,0 +1,10 @@
+
+
+function LoadScript()
+	
+	Menu = MenuElement({type = MENU, id = "PussyAIO".. myHero.charName, name = myHero.charName})
+	Menu:MenuElement({name = " ", drop = {"Graves is comming Soon"}})
+	
+
+		
+end
