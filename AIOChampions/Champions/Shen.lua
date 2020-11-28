066928572d1f4fa1adf610da7f1d3755f5774c1c
Pussykate <35004487+Pussykate@users.noreply.github.com>
2020-11-28 12:08:37 +0100

    Add files via upload

---
+++ AIOChampions/Champions/Shen.lua
@@ -0,0 +1,52 @@
+local function GetEnemyHeroes()
+	local _EnemyHeroes = {}
+	for i = 1, GameHeroCount() do
+		local unit = GameHero(i)
+		if unit.team ~= myHero.team then
+			TableInsert(_EnemyHeroes, unit)
+		end
+	end
+	return _EnemyHeroes
+end
+
+local function GetAllyHeroes()
+	local _AllyHeroes = {}
+	for i = 1, GameHeroCount() do
+		local unit = GameHero(i)
+		if unit.isAlly and not unit.isMe then
+			TableInsert(_AllyHeroes, unit)
+		end
+	end
+	return _AllyHeroes
+end
+
+local function HasBuff(unit, buffname)
+	for i = 0, unit.buffCount do
+		local buff = unit:GetBuff(i)
+		if buff.name == buffname and buff.count > 0 then 
+			return true
+		end
+	end
+	return false
+end
+
+local function IsUnderTurret(unit)
+    for i = 1, GameTurretCount() do
+        local turret = GameTurret(i)
+        local range = (turret.boundingRadius + 750 + unit.boundingRadius / 2)
+        if turret.isEnemy and not turret.dead then
+            if turret.pos:DistanceTo(unit.pos) < range then
+                return true
+            end
+        end
+    end
+    return false
+end
+
+local Ally1, Ally2, Ally3, Ally4 = nil, nil, nil, nil
+
+function LoadScript()
+	
+	Menu = MenuElement({type = MENU, id = "PussyAIO".. myHero.charName, name = myHero.charName})
+	Menu:MenuElement({name = " ", drop = {"Comming Soon"}})		
+end
