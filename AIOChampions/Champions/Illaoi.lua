322799b53e62d9907da2d707fb6ec98bc3047d93
Pussykate <35004487+Pussykate@users.noreply.github.com>
2020-12-23 21:53:28 +0100

    Update Illaoi.lua

--- AIOChampions/Champions/Illaoi.lua
+++ AIOChampions/Champions/Illaoi.lua
@@ -1,11 +1,21 @@
 
+local function GetEnemyHeroes()
+    local _EnemyHeroes = {}
+    for i = 1, GameHeroCount() do
+        local unit = GameHero(i)
+        if unit.isEnemy then
+            TableInsert(_EnemyHeroes, unit)
+        end
+    end
+    return _EnemyHeroes
+end 
+
 local function GetEnemyCount(range, pos)
     local pos = pos.pos
 	local count = 0
-	for i = 1, GameHeroCount() do 
-	local hero = GameHero(i)
+	for i, hero in ipairs(GetEnemyHeroes()) do
 	local Range = range * range
-		if hero.team ~= TEAM_ALLY and GetDistanceSqr(pos, hero.pos) < Range and IsValid(hero) then
+		if GetDistanceSqr(pos, hero.pos) < Range and IsValid(hero) then
 		count = count + 1
 		end
 	end
@@ -14,31 +24,27 @@ end
 
 function LoadScript()	
 	Menu = MenuElement({type = MENU, id = "PussyAIO".. myHero.charName, name = myHero.charName})
-	Menu:MenuElement({name = " ", drop = {"Version 0.02"}})
+	Menu:MenuElement({name = " ", drop = {"Version 0.03"}})
 	
 	--ComboMenu  
-	Menu:MenuElement({type = MENU, id = "Combo", name = "Combo"})
-	Menu.Combo:MenuElement({id = "Spirit", name = "Focus Spirit ?", value = true})	
+	Menu:MenuElement({type = MENU, id = "Combo", name = "Combo"})	
 	Menu.Combo:MenuElement({id = "UseQ", name = "[Q]", value = true})		
-	Menu.Combo:MenuElement({id = "UseW", name = "[W]", value = true})
-	Menu.Combo:MenuElement({id = "UseW2", name = "[W] only if Tentacles in range", value = true})	
+	Menu.Combo:MenuElement({id = "UseW", name = "[W]", value = true})	
 	Menu.Combo:MenuElement({id = "UseE", name = "[E]", value = true})
-	Menu.Combo:MenuElement({id = "ERange", name = "Max [E] range", value = 700, min = 0, max = 900})	
+	Menu.Combo:MenuElement({id = "ERange", name = "Max [E] range", value = 850, min = 0, max = 900})	
 	
 	--UltSettings
 	Menu.Combo:MenuElement({type = MENU, id = "Ult", name = "Ultimate Settings"})
 	Menu.Combo.Ult:MenuElement({id = "UseR", name = "[R]", value = true})
 	Menu.Combo.Ult:MenuElement({id = "UseRE", name = "[R] TargetCount", value = 2, min = 1, max = 5})
-	Menu.Combo.Ult:MenuElement({id = "UseRE2", name = "Count Spirit with TargetCount", value = true})	
+	Menu.Combo.Ult:MenuElement({id = "UseRE2", name = "[Count] Spirit + TargetCount", value = true})	
 
 	--HarassMenu
-	Menu:MenuElement({type = MENU, id = "Harass", name = "Harass"})
-	Menu.Harass:MenuElement({id = "Spirit", name = "Focus Spirit ?", value = true})	
+	Menu:MenuElement({type = MENU, id = "Harass", name = "Harass"})	
 	Menu.Harass:MenuElement({id = "UseQ", name = "[Q]", value = true})	
 	Menu.Harass:MenuElement({id = "UseW", name = "[W]", value = true})
-	Menu.Harass:MenuElement({id = "UseW2", name = "[W] only if Tentacles in range", value = true})
 	Menu.Harass:MenuElement({id = "UseE", name = "[E]", value = true})
-	Menu.Harass:MenuElement({id = "ERange", name = "Max [E] range", value = 700, min = 0, max = 900})	
+	Menu.Harass:MenuElement({id = "ERange", name = "Max [E] range", value = 850, min = 0, max = 900})	
 	Menu.Harass:MenuElement({id = "Mana", name = "Min Mana to Harass", value = 40, min = 0, max = 100, identifier = "%"})
   
 	--Prediction
@@ -87,31 +93,21 @@ function LoadScript()
 end
 
 local FoundSpirit = false
-local CastedE = false
-local Spirit = {}
-local Tentacles = {}
-local TentNear = false
+local Spirit = nil
 
 function Tick()
-RemoveTent()
+	if FoundSpirit then
+		RemoveSpirit()
+	end	
 
-if FoundSpirit then
-	RemoveSpirit()
-end	
-if CastedE then
-	ScanSpirit()
-end
-
-for i = 0, myHero.buffCount do
-	local buff = myHero:GetBuff(i)
-	if buff and buff.name == "illaoitentaclespawncooldown" and buff.duration > 6 and buff.duration < 9 then
-		CheckTent()
+	local currSpell = myHero.activeSpell
+	if currSpell and currSpell.valid and myHero.isChanneling and currSpell.name == "IllaoiE" then
+		ScanSpirit()
 	end
-end
 
-if MyHeroNotReady() then return end
+	if MyHeroNotReady() then return end
 
-local Mode = GetMode()
+	local Mode = GetMode()
 	if Mode == "Combo" then
 		Combo()
 	elseif Mode == "Harass" then
@@ -119,296 +115,157 @@ local Mode = GetMode()
 	end	
 end
 
-function CheckTent()	
-	for i = 0, GameObjectCount() do
-		local tent = GameObject(i)
-		local NewTent = true
-		if tent and myHero.pos:DistanceTo(tent.pos) < 1500 and tent.charName == "IllaoiMinion" then
-			for i = 1, #Tentacles do
-				if Tentacles[i].networkID == tent.networkID then
-					NewTent = false
-				end
-			end				
-			
-			if NewTent then 
-				if tent.charName == "IllaoiMinion" and myHero.pos:DistanceTo(tent.pos) < 1500 then
-					--print("FoundTent")
-					TableInsert(Tentacles, tent)
-				end	
-			end	
-		end
-	end	
-end
-
-function RemoveTent()
-	for i = 1, #Tentacles do
-		local Tent = Tentacles[i] 
-		if Tent and myHero.pos:DistanceTo(Tent.pos) <= 1500 and (Tent.health < 1 or Tent.charName ~= "IllaoiMinion") then				
-			TableRemove(Tentacles, i)
-			--print("Removed")
-		end	
-	end
-end
-
 function ScanSpirit()
 	if FoundSpirit then return end
 	DelayAction(function()
 		for i = 0, GameObjectCount() do
 			local obj = GameObject(i)
-			if obj and myHero.pos:DistanceTo(obj.pos) < 600 and obj.name == "Illaoi_Base_E_Spirit" then				
+			if obj and myHero.pos:DistanceTo(obj.pos) < 600 and obj.name == "Illaoi_Base_E_Spirit" then								
 				--print("FoundSpirit")
 				FoundSpirit = true
-				CastedE = false
-				TableInsert(Spirit, obj)
-				return
+				Spirit = obj
 			end
 		end
-	end,0.25)	
+	end,0.5)	
 end
 
 function RemoveSpirit()
-	for i = 1, #Spirit do
-		local Obj = Spirit[i] 
-		if Obj and not Obj.visible then				
-			TableRemove(Spirit, i)
-			FoundSpirit = false
-			--print("Removed")
-		end
-	end
+	local Obj = Spirit
+	if not Obj.visible or not Obj.pos2D.onScreen or Obj.name ~= "Illaoi_Base_E_Spirit" then
+		Spirit = nil
+		FoundSpirit = false
+		--print("Removed")								
+	--else	
+		--DrawCircle(Obj.pos, 100, 1, DrawColor(255, 225, 255, 10)) 
+	end	
 end
 
-local SpiritNear = false
-function Combo()
-	if Menu.Combo.Spirit:Value() and FoundSpirit then
-		for i = 1, #Spirit do
-			local Obj = Spirit[i]
-			if Obj and myHero.pos:DistanceTo(Obj.pos) < 825 then
-				SpiritNear = true
-				
-				if myHero.pos:DistanceTo(Obj.pos) <= 350 then
-					--Control.Attack(Obj)
+function Combo()		
+	local target = GetTarget(900)
+	if target == nil then return end
+	if IsValid(target) then
+	
+		if Menu.Combo.UseE:Value() and myHero.pos:DistanceTo(target.pos) <= Menu.Combo.ERange:Value() and Ready(_E) then
+			if Menu.Pred.Change:Value() == 1 then
+				local pred = GetGamsteronPrediction(target, EData, myHero)
+				if pred.Hitchance >= Menu.Pred.PredE:Value()+1 then
+					Control.CastSpell(HK_E, pred.CastPosition)
+					return
 				end
-				
-				if Menu.Combo.UseQ:Value() and myHero.pos:DistanceTo(Obj.pos) < 825 and Ready(_Q) then
-					if Menu.Pred.Change:Value() == 1 then
-						local pred = GetGamsteronPrediction(Obj, QData, myHero)
-						if pred.Hitchance >= Menu.Pred.PredQ:Value()+1 then
-							Control.CastSpell(HK_Q, pred.CastPosition)
-						end
-					elseif Menu.Pred.Change:Value() == 2 then
-						local pred = _G.PremiumPrediction:GetPrediction(myHero, Obj, QspellData)
-						if pred.CastPos and ConvertToHitChance(Menu.Pred.PredQ:Value(), pred.HitChance) then
-							Control.CastSpell(HK_Q, pred.CastPos)
-						end
-					else
-						CastGGPred(_Q, Obj)
-					end
+			elseif Menu.Pred.Change:Value() == 2 then
+				local pred = _G.PremiumPrediction:GetPrediction(myHero, target, EspellData)
+				if pred.CastPos and ConvertToHitChance(Menu.Pred.PredE:Value(), pred.HitChance) then
+					Control.CastSpell(HK_E, pred.CastPos)
+					return
 				end
-			   
-				if Menu.Combo.UseW:Value() and myHero.pos:DistanceTo(Obj.pos) <= 450 and Ready(_W) then
-					Control.CastSpell(HK_W)
-					Control.Attack(Obj)
+			else
+				local EPrediction = GGPrediction:SpellPrediction({Type = GGPrediction.SPELLTYPE_LINE, Delay = (0.25+ping), Radius = 50, Range = 900, Speed = 1900, Collision = true, CollisionTypes = {GGPrediction.COLLISION_MINION}})
+				EPrediction:GetPrediction(target, myHero)
+				if EPrediction:CanHit(Menu.Pred.PredE:Value() + 1) then
+					Control.CastSpell(HK_E, EPrediction.CastPosition)
+					return
 				end
+			end
+		end
 
-				if Menu.Combo.Ult.UseR:Value() and Ready(_R) then
-					local count = GetEnemyCount(450, myHero)
-					if Menu.Combo.Ult.UseRE2:Value() then
-						if myHero.pos:DistanceTo(Obj.pos) < 450 then
-							if count+1 >= Menu.Combo.Ult.UseRE:Value() then
-								Control.CastSpell(HK_R)
-							end	
-						else
-							if count >= Menu.Combo.Ult.UseRE:Value() then
-								Control.CastSpell(HK_R)
-							end	
-						end
-					else
-						if count >= Menu.Combo.Ult.UseRE:Value() then
-							Control.CastSpell(HK_R)
-						end						
+		if Menu.Combo.UseW:Value() and myHero.pos:DistanceTo(target.pos) <= 400 and Ready(_W) then
+			Control.CastSpell(HK_W)
+			Control.Attack(target)
+		end			
+		
+		if Menu.Combo.UseQ:Value() and myHero.pos:DistanceTo(target.pos) < 800 and Ready(_Q) then
+			if Menu.Pred.Change:Value() == 1 then
+				local pred = GetGamsteronPrediction(target, QData, myHero)
+				if pred.Hitchance >= Menu.Pred.PredQ:Value()+1 then
+					Control.CastSpell(HK_Q, pred.CastPosition)
+				end
+			elseif Menu.Pred.Change:Value() == 2 then
+				local pred = _G.PremiumPrediction:GetPrediction(myHero, target, QspellData)
+				if pred.CastPos and ConvertToHitChance(Menu.Pred.PredQ:Value(), pred.HitChance) then
+					Control.CastSpell(HK_Q, pred.CastPos)
+				end
+			else
+				local QPrediction = GGPrediction:SpellPrediction({Type = GGPrediction.SPELLTYPE_LINE, Delay = (0.75+ping), Radius = 100, Range = 825, Speed = MathHuge, Collision = false})
+				QPrediction:GetPrediction(target, myHero)
+				if QPrediction:CanHit(Menu.Pred.PredQ:Value() + 1) then
+					Control.CastSpell(HK_Q, QPrediction.CastPosition)
+				end
+			end
+		end
+
+		if Menu.Combo.Ult.UseR:Value() and Ready(_R) then
+			local Obj = Spirit
+			local count = GetEnemyCount(450, myHero)
+			if Menu.Combo.Ult.UseRE2:Value() and Obj then
+				if myHero.pos:DistanceTo(Obj.pos) < 450 then
+					if count+1 >= Menu.Combo.Ult.UseRE:Value() then
+						Control.CastSpell(HK_R)
+					end	
+				else
+					if count >= Menu.Combo.Ult.UseRE:Value() then
+						Control.CastSpell(HK_R)
 					end	
 				end
 			else
-				SpiritNear = false
-			end
-		end
-	end	
-	
-	if (((Menu.Combo.Spirit:Value() and not FoundSpirit) or (Menu.Combo.Spirit:Value() and FoundSpirit and not SpiritNear)) or (not Menu.Combo.Spirit:Value())) then
-		local target = GetTarget(900)
-		if target == nil then return end
-		if IsValid(target) then
-		
-			if Menu.Combo.UseE:Value() and myHero.pos:DistanceTo(target.pos) <= Menu.Combo.ERange:Value() and Ready(_E) then
-				if Menu.Pred.Change:Value() == 1 then
-					local pred = GetGamsteronPrediction(target, EData, myHero)
-					if pred.Hitchance >= Menu.Pred.PredE:Value()+1 then
-						Control.CastSpell(HK_E, pred.CastPosition)
-						CastedE = true
-					end
-				elseif Menu.Pred.Change:Value() == 2 then
-					local pred = _G.PremiumPrediction:GetPrediction(myHero, target, EspellData)
-					if pred.CastPos and ConvertToHitChance(Menu.Pred.PredE:Value(), pred.HitChance) then
-						Control.CastSpell(HK_E, pred.CastPos)
-						CastedE = true
-					end
-				else
-					CastGGPred(_E, target)
-				end
-			end	
-			
-			if Menu.Combo.UseQ:Value() and myHero.pos:DistanceTo(target.pos) < 825 and Ready(_Q) then
-				if Menu.Pred.Change:Value() == 1 then
-					local pred = GetGamsteronPrediction(target, QData, myHero)
-					if pred.Hitchance >= Menu.Pred.PredQ:Value()+1 then
-						Control.CastSpell(HK_Q, pred.CastPosition)
-					end
-				elseif Menu.Pred.Change:Value() == 2 then
-					local pred = _G.PremiumPrediction:GetPrediction(myHero, target, QspellData)
-					if pred.CastPos and ConvertToHitChance(Menu.Pred.PredQ:Value(), pred.HitChance) then
-						Control.CastSpell(HK_Q, pred.CastPos)
-					end
-				else
-					CastGGPred(_Q, target)
-				end
-			end
-		   
-			if Menu.Combo.UseW:Value() and myHero.pos:DistanceTo(target.pos) <= 450 and Ready(_W) then
-				if Menu.Combo.UseW2:Value() then
-					for i = 1, #Tentacles do
-						local Tent = Tentacles[i] 
-						if Tent and target.pos:DistanceTo(Tent.pos) < 850 then					
-							Control.CastSpell(HK_W)
-							Control.Attack(target)
-						end
-					end	
-				else
-					Control.CastSpell(HK_W)
-					Control.Attack(target)
-				end	
-			end
-
-			if Menu.Combo.Ult.UseR:Value() and Ready(_R) then
-				local count = GetEnemyCount(450, myHero)
 				if count >= Menu.Combo.Ult.UseRE:Value() then
 					Control.CastSpell(HK_R)
-				end	
-			end
+				end						
+			end	
 		end
 	end	
 end
 
 function Harass()
-	if Menu.Harass.Spirit:Value() and FoundSpirit then
-		for i = 1, #Spirit do
-			local Obj = Spirit[i]
-			if Obj and myHero.pos:DistanceTo(Obj.pos) < 825 then
-				SpiritNear = true
-				
-				if myHero.pos:DistanceTo(Obj.pos) <= 350 then
-					--Control.Attack(Obj)
+	local target = GetTarget(900)
+	if target == nil then return end
+	if IsValid(target) then
+	
+		if Menu.Harass.UseE:Value() and myHero.pos:DistanceTo(target.pos) <= Menu.Harass.ERange:Value() and Ready(_E) then
+			if Menu.Pred.Change:Value() == 1 then
+				local pred = GetGamsteronPrediction(target, EData, myHero)
+				if pred.Hitchance >= Menu.Pred.PredE:Value()+1 then
+					Control.CastSpell(HK_E, pred.CastPosition)
+					return
 				end
-				
-				if Menu.Harass.UseQ:Value() and myHero.pos:DistanceTo(Obj.pos) < 825 and Ready(_Q) then
-					if Menu.Pred.Change:Value() == 1 then
-						local pred = GetGamsteronPrediction(Obj, QData, myHero)
-						if pred.Hitchance >= Menu.Pred.PredQ:Value()+1 then
-							Control.CastSpell(HK_Q, pred.CastPosition)
-						end
-					elseif Menu.Pred.Change:Value() == 2 then
-						local pred = _G.PremiumPrediction:GetPrediction(myHero, Obj, QspellData)
-						if pred.CastPos and ConvertToHitChance(Menu.Pred.PredQ:Value(), pred.HitChance) then
-							Control.CastSpell(HK_Q, pred.CastPos)
-						end
-					else
-						CastGGPred(_Q, Obj)
-					end
-				end
-			   
-				if Menu.Harass.UseW:Value() and myHero.pos:DistanceTo(Obj.pos) <= 450 and Ready(_W) then
-					Control.CastSpell(HK_W)
-					Control.Attack(Obj)
+			elseif Menu.Pred.Change:Value() == 2 then
+				local pred = _G.PremiumPrediction:GetPrediction(myHero, target, EspellData)
+				if pred.CastPos and ConvertToHitChance(Menu.Pred.PredE:Value(), pred.HitChance) then
+					Control.CastSpell(HK_E, pred.CastPos)
+					return
 				end
 			else
-				SpiritNear = false
+				local EPrediction = GGPrediction:SpellPrediction({Type = GGPrediction.SPELLTYPE_LINE, Delay = (0.25+ping), Radius = 50, Range = 900, Speed = 1900, Collision = true, CollisionTypes = {GGPrediction.COLLISION_MINION}})
+				EPrediction:GetPrediction(target, myHero)
+				if EPrediction:CanHit(Menu.Pred.PredE:Value() + 1) then
+					Control.CastSpell(HK_E, EPrediction.CastPosition)
+					return
+				end
 			end
 		end
-	end	
-	
-	if (((Menu.Harass.Spirit:Value() and not FoundSpirit) or (Menu.Harass.Spirit:Value() and FoundSpirit and not SpiritNear)) or (not Menu.Harass.Spirit:Value())) then
-		local target = GetTarget(900)
-		if target == nil then return end
-		if IsValid(target) then
+
+		if Menu.Harass.UseW:Value() and myHero.pos:DistanceTo(target.pos) <= 400 and Ready(_W) then
+			Control.CastSpell(HK_W)
+			Control.Attack(target)	
+		end			
 		
-			if Menu.Harass.UseE:Value() and myHero.pos:DistanceTo(target.pos) <= Menu.Harass.ERange:Value() and Ready(_E) then
-				if Menu.Pred.Change:Value() == 1 then
-					local pred = GetGamsteronPrediction(target, EData, myHero)
-					if pred.Hitchance >= Menu.Pred.PredE:Value()+1 then
-						Control.CastSpell(HK_E, pred.CastPosition)
-						CastedE = true
-					end
-				elseif Menu.Pred.Change:Value() == 2 then
-					local pred = _G.PremiumPrediction:GetPrediction(myHero, target, EspellData)
-					if pred.CastPos and ConvertToHitChance(Menu.Pred.PredE:Value(), pred.HitChance) then
-						Control.CastSpell(HK_E, pred.CastPos)
-						CastedE = true
-					end
-				else
-					CastGGPred(_E, target)
+		if Menu.Harass.UseQ:Value() and myHero.pos:DistanceTo(target.pos) < 800 and Ready(_Q) then
+			if Menu.Pred.Change:Value() == 1 then
+				local pred = GetGamsteronPrediction(target, QData, myHero)
+				if pred.Hitchance >= Menu.Pred.PredQ:Value()+1 then
+					Control.CastSpell(HK_Q, pred.CastPosition)
 				end
-			end	
-			
-			if Menu.Harass.UseQ:Value() and myHero.pos:DistanceTo(target.pos) < 825 and Ready(_Q) then
-				if Menu.Pred.Change:Value() == 1 then
-					local pred = GetGamsteronPrediction(target, QData, myHero)
-					if pred.Hitchance >= Menu.Pred.PredQ:Value()+1 then
-						Control.CastSpell(HK_Q, pred.CastPosition)
-					end
-				elseif Menu.Pred.Change:Value() == 2 then
-					local pred = _G.PremiumPrediction:GetPrediction(myHero, target, QspellData)
-					if pred.CastPos and ConvertToHitChance(Menu.Pred.PredQ:Value(), pred.HitChance) then
-						Control.CastSpell(HK_Q, pred.CastPos)
-					end
-				else
-					CastGGPred(_Q, target)
+			elseif Menu.Pred.Change:Value() == 2 then
+				local pred = _G.PremiumPrediction:GetPrediction(myHero, target, QspellData)
+				if pred.CastPos and ConvertToHitChance(Menu.Pred.PredQ:Value(), pred.HitChance) then
+					Control.CastSpell(HK_Q, pred.CastPos)
 				end
-			end
-		   
-			if Menu.Harass.UseW:Value() and myHero.pos:DistanceTo(target.pos) <= 450 and Ready(_W) then
-				if Menu.Harass.UseW2:Value() then
-					for i = 1, #Tentacles do
-						local Tent = Tentacles[i] 
-						if Tent and target.pos:DistanceTo(Tent.pos) < 850 then					
-							Control.CastSpell(HK_W)
-							Control.Attack(target)
-						end
-					end	
-				else
-					Control.CastSpell(HK_W)
-					Control.Attack(target)
-				end	
+			else
+				local QPrediction = GGPrediction:SpellPrediction({Type = GGPrediction.SPELLTYPE_LINE, Delay = (0.75+ping), Radius = 100, Range = 825, Speed = MathHuge, Collision = false})
+				QPrediction:GetPrediction(target, myHero)
+				if QPrediction:CanHit(Menu.Pred.PredQ:Value() + 1) then
+					Control.CastSpell(HK_Q, QPrediction.CastPosition)
+				end
 			end
 		end
 	end	
 end	
-
-function CastGGPred(spell, unit)
-	if spell == _Q then
-		local QPrediction = GGPrediction:SpellPrediction({Type = GGPrediction.SPELLTYPE_LINE, Delay = (0.75+ping), Radius = 100, Range = 825, Speed = MathHuge, Collision = false})
-		QPrediction:GetPrediction(unit, myHero)
-		if QPrediction:CanHit(Menu.Pred.PredQ:Value() + 1) then
-			Control.CastSpell(HK_Q, QPrediction.CastPosition)
-		end	
-	
-	else
-	
-		if spell == _E then
-			local EPrediction = GGPrediction:SpellPrediction({Type = GGPrediction.SPELLTYPE_LINE, Delay = (0.25+ping), Radius = 50, Range = 900, Speed = 1900, Collision = true, CollisionTypes = {GGPrediction.COLLISION_MINION}})
-			EPrediction:GetPrediction(unit, myHero)
-			if EPrediction:CanHit(Menu.Pred.PredE:Value() + 1) then
-				Control.CastSpell(HK_E, EPrediction.CastPosition)
-				CastedE = true
-			end	
-		end	
-	end
-end
