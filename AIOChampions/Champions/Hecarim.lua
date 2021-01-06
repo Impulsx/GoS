05a3a2a2f3857fd7b5a0aa8fec0f552a548dc5af
Pussykate <35004487+Pussykate@users.noreply.github.com>
2021-01-06 14:45:48 +0100

    Update Hecarim.lua

--- AIOChampions/Champions/Hecarim.lua
+++ AIOChampions/Champions/Hecarim.lua
@@ -1,1 +1,4 @@
-1
+function LoadScript()	
+	Menu = MenuElement({type = MENU, id = "PussyAIO".. myHero.charName, name = myHero.charName})
+	Menu:MenuElement({name = " ", drop = {"Comming Soon"}})
+end
