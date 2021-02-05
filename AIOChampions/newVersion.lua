c1e8bc2f0cf061d4b0ee179cdedc3a76ce204b38
Pussykate <35004487+Pussykate@users.noreply.github.com>
2021-02-05 12:48:21 +0100

    Update newVersion.lua

--- AIOChampions/newVersion.lua
+++ AIOChampions/newVersion.lua
@@ -55,7 +55,7 @@ Data = {
         },
         Tristana = {
             Changelog = "Tristana Changelog Here",
-            Version = 0.10,
+            Version = 0.11,
         }, 
         Sona = {
             Changelog = "Sona Changelog Here",
@@ -283,7 +283,7 @@ Data = {
         }, 
            Xayah = {
             Changelog = "Xayah Changelog Here",
-          Version = 0.01,
+          Version = 0.02,
         },         
     },
 }
