f6c44f4bab0afc8782fe6c0c1c9c41eab9bd8bd5
Pussykate <35004487+Pussykate@users.noreply.github.com>
2021-02-19 14:39:35 +0100

    Update Veigar.lua

--- AIOChampions/Champions/Veigar.lua
+++ AIOChampions/Champions/Veigar.lua
@@ -14,7 +14,7 @@ end
 local function IsImmobileTarget(unit)
 	for i = 0, unit.buffCount do
 		local buff = unit:GetBuff(i)
-		if buff and (buff.type == 5 or buff.type == 11 or buff.type == 29 or buff.type == 24 or buff.name == 10 ) and buff.count > 0 then
+		if buff and (buff.type == 5 or buff.type == 11 or buff.type == 29 or buff.type == 24 or buff.type == 10 ) and buff.count > 0 then
 			return true
 		end
 	end
@@ -26,7 +26,7 @@ end
 function LoadScript()
 
 	Menu = MenuElement({type = MENU, id = "PussyAIO".. myHero.charName, name = myHero.charName})
-	Menu:MenuElement({name = " ", drop = {"Version 0.06"}})	
+	Menu:MenuElement({name = " ", drop = {"Version 0.07"}})	
 	
 	Menu:MenuElement({id = "Combo", name = "Combo", type = MENU})
 	Menu.Combo:MenuElement({id = "UseQ", name = "Q", value = true})
