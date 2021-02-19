1906fab2a7d7b8876b9eff6f1bbec00668b2c1c4
Pussykate <35004487+Pussykate@users.noreply.github.com>
2021-02-19 14:38:11 +0100

    Update Sylas.lua

--- AIOChampions/Champions/Sylas.lua
+++ AIOChampions/Champions/Sylas.lua
@@ -1,7 +1,7 @@
 local function IsImmobileTarget(unit)
 	for i = 0, unit.buffCount do
 		local buff = unit:GetBuff(i)
-		if buff and (buff.type == 5 or buff.type == 11 or buff.type == 29 or buff.type == 24 or buff.name == 10 ) and buff.count > 0 then
+		if buff and (buff.type == 5 or buff.type == 11 or buff.type == 29 or buff.type == 24 or buff.type == 10 ) and buff.count > 0 then
 			return true
 		end
 	end
@@ -127,7 +127,7 @@ local UltSpells = {
 function LoadScript()
 	HPred()	
 	Menu = MenuElement({type = MENU, id = "PussyAIO".. myHero.charName, name = myHero.charName})
-	Menu:MenuElement({name = " ", drop = {"Version 0.07"}})	
+	Menu:MenuElement({name = " ", drop = {"Version 0.08"}})	
 
 	--AutoW
 	Menu:MenuElement({type = MENU, id = "AutoW", name = "AutoW"})	
