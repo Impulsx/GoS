a9eafc1e4f82fe8dd7b426bb0187f457d1712339
Pussykate <35004487+Pussykate@users.noreply.github.com>
2021-02-19 14:41:57 +0100

    Update Volibear.lua

--- AIOChampions/Champions/Volibear.lua
+++ AIOChampions/Champions/Volibear.lua
@@ -162,7 +162,7 @@ end
 local function IsImmobileTarget(unit)
 	for i = 0, unit.buffCount do
 		local buff = unit:GetBuff(i)
-		if buff and (buff.type == 5 or buff.type == 11 or buff.type == 29 or buff.type == 24 or buff.name == 10 ) and buff.count > 0 then
+		if buff and (buff.type == 5 or buff.type == 11 or buff.type == 29 or buff.type == 24 or buff.type == 10 ) and buff.count > 0 then
 			return true
 		end
 	end
@@ -267,7 +267,7 @@ function LoadScript()
 	CanUlt = false	
 	
 	Menu = MenuElement({type = MENU, id = "PussyAIO".. myHero.charName, name = myHero.charName})
-	Menu:MenuElement({name = " ", drop = {"Version 0.03"}})		
+	Menu:MenuElement({name = " ", drop = {"Version 0.04"}})		
 	
 	--AutoQ	
 	Menu:MenuElement({type = MENU, id = "QSet", name = "AutoQ"})	
