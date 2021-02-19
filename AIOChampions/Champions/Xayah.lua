7f8cc2a136fc1ccd1284d410776da95f887cd9e2
Pussykate <35004487+Pussykate@users.noreply.github.com>
2021-02-19 14:43:03 +0100

    Update Xayah.lua

--- AIOChampions/Champions/Xayah.lua
+++ AIOChampions/Champions/Xayah.lua
@@ -12,7 +12,7 @@ end
 local function IsImmobileTarget(unit)
 	for i = 0, unit.buffCount do
 		local buff = unit:GetBuff(i)
-		if buff and (buff.type == 5 or buff.type == 11 or buff.type == 29 or buff.type == 24 or buff.name == 10 ) and buff.count > 0 then
+		if buff and (buff.type == 5 or buff.type == 11 or buff.type == 29 or buff.type == 24 or buff.type == 10 ) and buff.count > 0 then
 			return true
 		end
 	end
@@ -105,7 +105,7 @@ end
 function LoadScript() 
 
 	Menu = MenuElement({type = MENU, id = "PussyAIO".. myHero.charName, name = myHero.charName})
-	Menu:MenuElement({name = " ", drop = {"Version 0.04"}})			
+	Menu:MenuElement({name = " ", drop = {"Version 0.05"}})			
 		
 	--ComboMenu  
 	Menu:MenuElement({type = MENU, id = "Combo", name = "Combo"})
