3fbeec834da5e01a54f8577923e704471c6c9271
Pussykate <35004487+Pussykate@users.noreply.github.com>
2021-02-19 14:27:55 +0100

    Update Malzahar.lua

--- AIOChampions/Champions/Malzahar.lua
+++ AIOChampions/Champions/Malzahar.lua
@@ -14,7 +14,7 @@ end
 local function IsImmobileTarget(unit)
 	for i = 0, unit.buffCount do
 		local buff = unit:GetBuff(i)
-		if buff and (buff.type == 5 or buff.type == 11 or buff.type == 29 or buff.type == 24 or buff.name == 10 ) and buff.count > 0 then
+		if buff and (buff.type == 5 or buff.type == 11 or buff.type == 29 or buff.type == 24 or buff.type == 10 ) and buff.count > 0 then
 			return true
 		end
 	end
@@ -54,7 +54,7 @@ end
 
 function LoadScript()
 	Menu = MenuElement({type = MENU, id = "PussyAIO".. myHero.charName, name = myHero.charName})
-	Menu:MenuElement({name = " ", drop = {"Version 0.09"}})	
+	Menu:MenuElement({name = " ", drop = {"Version 0.10"}})	
 
 	--AutoQ
 	Menu:MenuElement({type = MENU, id = "AutoQ", name = "Auto[Q]Immobile"})
