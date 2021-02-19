322c27573e503298961023a0b6a4e8ca3e4b8c06
Pussykate <35004487+Pussykate@users.noreply.github.com>
2021-02-19 14:45:25 +0100

    Update Zyra.lua

--- AIOChampions/Champions/Zyra.lua
+++ AIOChampions/Champions/Zyra.lua
@@ -2,7 +2,7 @@
 local function IsImmobileTarget(unit)
 	for i = 0, unit.buffCount do
 		local buff = unit:GetBuff(i)
-		if buff and (buff.type == 5 or buff.type == 11 or buff.type == 29 or buff.type == 24 or buff.name == 10 ) and buff.count > 0 then
+		if buff and (buff.type == 5 or buff.type == 11 or buff.type == 29 or buff.type == 24 or buff.type == 10 ) and buff.count > 0 then
 			return true
 		end
 	end
@@ -38,7 +38,7 @@ end
 function LoadScript()
 	
 	Menu = MenuElement({type = MENU, id = "PussyAIO".. myHero.charName, name = myHero.charName})
-	Menu:MenuElement({name = " ", drop = {"Version 0.09"}})	
+	Menu:MenuElement({name = " ", drop = {"Version 0.10"}})	
 	
 	Menu:MenuElement({type = MENU, id = "E", name = "E range Setting"})
 	Menu.E:MenuElement({id = "Erange", name = "Max E range", value = 900, min = 100, max = 1100, step = 10})	
