3055810981a41b5355392c2272651d8be8b7b991
Impulsx <idqdmaster@yahoo.com>
2021-03-15 12:15:35 -0500

    Champ revert v2

--- AIOChampions/newVersion.lua
+++ AIOChampions/newVersion.lua
@@ -217,6 +217,10 @@ Data = {
             Changelog = "Kayn Changelog Here",
             Version = 0.02,
         },
+        Kayle = {
+            Changelog = "Kayle Changelog Here",
+            Version = 0.04,
+        },
          Graves = {
             Changelog = "Graves Changelog Here",
             Version = 0.05,
